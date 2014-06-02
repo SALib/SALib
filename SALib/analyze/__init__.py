0b5bd6cff944f902d8972b306825da653ec36a93
zoidy <zoidy@users.noreply.github.com>
2014-06-02 09:47:59 -0400

    Improve importing of modules
    modified a couple __init__.py's to allow for importing SALib via
    import SALib.sample as x
    import SALib.util as y
    import SALib.analyze as z

--- SALib/analyze/__init__.py
+++ SALib/analyze/__init__.py
@@ -1,1 +1,4 @@
+import extended_fast
+import morris
+import sobol
 __all__ = ["sobol", "morris", "extended_fast"]
--- SALib/sample/__init__.py
+++ SALib/sample/__init__.py
@@ -1,1 +1,7 @@
+import morris_oat
+import uniform
+import normal
+import sobol
+import latin_hypercube
+import fast_sampler
 __all__ = ["uniform", "normal", "latin_hypercube", "saltelli", "morris_oat", "fast_sampler"]
