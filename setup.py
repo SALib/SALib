7a057d427626976a7dddf618460f6d641a77de86
Chris Mutel <cmutel@gmail.com>
2014-10-02 09:08:59 +0200

    Add setup.py and manifest
    Create a normal python package

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+import os
+
+packages = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('SALib'):
+    # Ignore dirnames that start with '.'
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+
+setup(
+    name='salib',
+    version="0.1",
+    packages=packages,
+    author="Jon Herman",
+    author_email="jdh366@cornell.edu",
+    license=open('LICENSE.md').read(),
+    install_requires=[
+        "docopt",
+        "numpy",
+        "scipy",
+        "scikit-learn",
+    ],
+    url="https://github.com/jdherman/SALib",
+    long_description=open('README.md').read(),
+    description=('Tools for sensitivity analysis. Contains Sobol, Morris, and FAST methods.'),
+    # entry_points = {
+    #     'console_scripts': [
+    #         'salib = SALib.bin.salib:main',
+    #     ]
+    # },
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        # 'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],)
