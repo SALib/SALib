60742e4ab7331910eb9224d784b1d94a55e24646
Will Usher <w.usher@ucl.ac.uk>
2014-10-05 23:24:01 +0100

    Added tests and optimised distance summation
    KEY CHANGE - included from__future__ import division (essential to
    correct operation of the morris_sample() function.
    Changed args to morris_sample so that bounds and num_vars are
    arguments, rather than calling the read_file procedure.
    Optimised the private function Map() by using np.einsum() to conduct
    sum(square(distance)) resulting in 3-4x speedup.
    Wrapped whole optimisation of trajectories in new function
    find_optimum_trajectories(), which returns the k_chioces number of
    trajectories from an input sample size of N.
    Started on a class OptimisedTrajectories() to wrap the whole lot in.
    Added parser and arguments for running the esme.py file as a module
    from the shell.
    Added tests for the new procedures.

--- SALib/esme.py
+++ SALib/esme.py
@@ -1,22 +1,32 @@
+from __future__ import division
 import numpy as np
 import random as rd
 from math import factorial
 from itertools import combinations
 from util import scale_samples, read_param_file
-from multiprocessing import Pool, Array
+from sample import common_args
 
-# Generate N(D + 1) x D matrix of Morris samples (OAT)
-def sample(N, param_file, num_levels, grid_jump):
 
-    pf = read_param_file(param_file)
-    D = pf['num_vars']
+def morris_sample(N, num_params, bounds, num_levels, grid_jump):
+    '''
+    Generates N('num_params' + 1) x 'num_params' matrix of Morris samples (OAT)
+    '''
+
+    if num_params < 1:
+        raise ValueError("num_params must be greater than or equal to 1")
+    if N < 1:
+        raise ValueError("N should be greater than 0")
+    if type(bounds) != "list":
+        TypeError("bounds should be a list")
+
+    D = num_params
 
     # orientation matrix B: lower triangular (1) + upper triangular (-1)
-    B = np.tril(np.ones([D+1, D], dtype=int), -1) + np.triu(-1*np.ones([D+1,D], dtype=int))
+    B = np.tril(np.ones([D+1, D], dtype=np.int), -1) + np.triu(-1*np.ones([D+1,D], dtype=np.int))
 
     # grid step delta, and final sample matrix X
     delta = grid_jump / (num_levels - 1)
-    X = np.empty([N*(D+1), D])
+    X = np.empty([N*(D+1), D], dtype=np.float32)
 
     # Create N trajectories. Each trajectory contains D+1 parameter sets.
     # (Starts at a base point, and then changes one parameter at a time)
@@ -27,12 +37,12 @@ def sample(N, param_file, num_levels, grid_jump):
 
         # permutation matrix P
         perm = np.random.permutation(D)
-        P = np.zeros([D,D])
+        P = np.zeros([D,D], dtype=np.float32)
         for i in range(D):
             P[i, perm[i]] = 1
 
         # starting point for this trajectory
-        x_base = np.empty([D+1, D])
+        x_base = np.empty([D+1, D], dtype=np.float32)
         for i in range(D):
             x_base[:,i] = (rd.choice(np.arange(num_levels - grid_jump))) / (num_levels - 1)
 
@@ -42,7 +52,7 @@ def sample(N, param_file, num_levels, grid_jump):
 
         X[index_list,:] = 0.5*(np.mat(B)*np.mat(P)*np.mat(DM) + 1) * np.mat(delta_diag) + np.mat(x_base)
 
-    scale_samples(X, pf['bounds'])
+    scale_samples(X, bounds)
     return X
 
 
@@ -85,7 +95,8 @@ def compute_distance_matrix(input_sample, N, num_params):
         for k in range(j + 1, N):
             input_1 = input_sample[index_list[j]]
             input_2 = input_sample[index_list[k]]
-            distance_matrix[k, j] = compute_distance(input_1, input_2, num_params)
+            distance_matrix[k, j] = compute_distance(input_1, input_2,
+                                                     num_params)
     return distance_matrix
 
 
@@ -98,8 +109,8 @@ def find_most_distant(input_sample, N, num_params, k_choices):
     # First compute the distance matrix for each possible pairing
     # of trajectories and store in a shared-memory array
     distance_matrix = compute_distance_matrix(input_sample,
-                                                       N,
-                                                       num_params)
+                                              N,
+                                              num_params)
 
     # Now evaluate the (N choose k_choices) possible combinations
     number_of_combinations = num_combinations(N, k_choices)
@@ -111,28 +122,53 @@ def find_most_distant(input_sample, N, num_params, k_choices):
     combos = [t for t in combinations(range(N), k_choices)]
 
     def Map(one_combination):
+        '''
+        Get a list of the pair-combinations from 'one_combination'
 
-        def square(x):
-            return np.square(distance_matrix[x[1], x[0]])
+        and return the spread, from the sum of absolute distance
+        of each combination
+        '''
 
-        return np.sqrt(reduce(add, map(square, combinations(one_combination, 2))))
+        list_of_pairs = combinations(one_combination, 2)
+
+        distances = [distance_matrix[x[1], x[0]] for x in list_of_pairs]
+
+        return np.sqrt(np.einsum('i,i', distances, distances))
 
     output = np.array(map(Map, combos), dtype=np.float32)
 
     return output, combos
-    #for counter, t in enumerate(combos):
-    #    output[counter] = reduce(add,
-    #                             map(lambda x:
-    #                                    np.square(distance_matrix[x[1], x[0]]),
-    #                                 combinations(t, 2))
-    #                            )
-
-def add(x,y):
-    return x + y
-
-class optimised_trajectories(object):
 
 
+def find_maximum(scores, combinations):
+
+    if type(scores) != np.ndarray:
+        raise TypeError("Scores input is not a numpy array")
+
+    index_of_maximum = scores.argmax()
+    print scores[index_of_maximum]
+    return combinations[index_of_maximum]
+
+
+def find_optimimum_trajectories(input_sample, N, num_params, k_choices):
+
+    scores, combinations = find_most_distant(input_sample,
+                                             N,
+                                             num_params,
+                                             k_choices)
+    index_of_maximum = scores.argmax()
+    index_list = []
+    for j in range(N):
+        index_list.append(np.arange(num_params + 1) + j * (num_params + 1))
+
+    maximum_combo = combinations[index_of_maximum]
+    output = np.zeros((np.size(maximum_combo) * (num_params + 1), num_params))
+    for counter, x in enumerate(maximum_combo):
+        output[index_list[counter]] = np.array(input_sample[index_list[x]])
+    return output
+
+
+class OptimisedTrajectories(object):
 
     def __init__(self, N, param_file, num_levels, grid_jump):
         self.N = N
@@ -140,37 +176,61 @@ class optimised_trajectories(object):
 
         pf = read_param_file(param_file)
         self.num_params = pf['num_vars']
+        self.bounds = pf['bounds']
 
         self.num_levels = num_levels
         self.grid_jump = grid_jump
 
         self.k_choices = k_choices
-        self.sample_inputs = self.sample(self.N, self.param_file, self.num_levels, self.grid_jump)
+        self.sample_inputs = self.morris_sample(self.N,
+                                         self.num_params,
+                                         self.bounds,
+                                         self.num_levels,
+                                         self.grid_jump)
         self.distance_matrix = \
             np.array(compute_distance_matrix(self.sample_inputs,
                                              self.N,
                                              self.num_params), dtype=np.float32)
 
+        self.optimised_inputs = \
+                        self.find_optimimum_trajectories(self.sample_inputs,
+                                                        self.N,
+                                                        self.num_params,
+                                                        self.k_choices)
 
-
-def find_maximum(scores, combinations):
-    index_of_maximum = scores.argmax()
-    print scores[index_of_maximum]
-    return combinations[index_of_maximum]
 
 
 if __name__ == "__main__":
-    param_file = 'esme_param.txt'
+
+
+    parser = common_args.create()
+    parser.add_argument('--num-levels', type=int, required=False, default=4, help='Number of grid levels (Morris only)')
+    parser.add_argument('--grid-jump', type=int, required=False, default=2, help='Grid jump size (Morris only)')
+    args = parser.parse_args()
+
+
+    np.random.seed(args.seed)
+    rd.seed(args.seed)
+
+
+    param_file = args.paramfile
     pf = read_param_file(param_file)
     N = 6
     num_params = pf['num_vars']
+    bounds = pf['bounds']
+    print bounds
     k_choices = 4
-    p_levels = 4
-    grid_step = 2
+    p_levels = int(args.num_levels)
+    grid_step = int(args.grid_jump)
     # Generates N(D + 1) x D matrix of samples
-    input_data = sample(N,
-                                   param_file,
-                                   num_levels=p_levels,
-                                   grid_jump=grid_step)
-    scores, combos = find_most_distant(input_data, N, num_params, k_choices)
-    print find_maximum(scores, combos)
+    input_data = morris_sample(N,
+                        num_params,
+                        bounds,
+                        num_levels=p_levels,
+                        grid_jump=grid_step)
+    print input_data
+    model_data = find_optimimum_trajectories(input_data, N, num_params, k_choices)
+    print model_data
+    np.savetxt(args.output, model_data, delimiter=' ')
+
+    #np.savetxt(args.output, model_data, delimiter=args.delimiter, fmt='%.' + str(args.precision) + 'e')
--- SALib/tests/test_esme.py
+++ SALib/tests/test_esme.py
@@ -1,12 +1,18 @@
 
 from nose.tools import assert_almost_equal, assert_equal
 from ..esme import compute_distance, compute_distance_matrix, \
-    find_most_distant, find_maximum#, Map
+    find_most_distant, find_maximum, find_optimimum_trajectories, \
+    morris_sample#, Map
+from ..sample.morris_oat import sample
+from ..util import read_param_file
 import numpy as np
-from itertools import combinations
+#from itertools import combinations
 
 
 def test_distance():
+    '''
+    Tests the computation of the distance of two trajectories
+    '''
     input_1 = np.matrix([[0, 1/3.], [0, 1.], [2/3., 1.]], dtype=np.float32)
     input_3 = np.matrix([[2/3., 0], [2/3., 2/3.], [0, 2/3.]], dtype=np.float32)
     output = compute_distance(input_1, input_3, 2)
@@ -25,10 +31,15 @@ def test_distance_fail_with_difference_size_ip():
 
 
 def test_compute_distance_matrix():
+    '''
+    Tests that a distance matrix is computed correctly
+
+    for an input of six trajectories and two parameters
+    '''
     sample_inputs = setUp()
     output = compute_distance_matrix(sample_inputs, 6, 2)
-    expected = np.zeros((6, 6))
-    expected[1, :] = [5.5, 0, 0, 0, 0, 0]
+    expected = np.zeros((6, 6), dtype=np.float32)
+    expected[1, :] = [5.50, 0, 0, 0, 0, 0]
     expected[2, :] = [6.18, 5.31, 0, 0, 0, 0]
     expected[3, :] = [6.89, 6.18, 6.57, 0, 0, 0]
     expected[4, :] = [6.18, 5.31, 5.41, 5.5, 0, 0]
@@ -59,7 +70,7 @@ def test_combo_from_find_most_distant():
     k_choices = 4
     scores, combinations = find_most_distant(sample_inputs, N, num_params, k_choices)
     output = find_maximum(scores, combinations)
-    expected = (0, 2, 3, 5)
+    expected = (0, 2, 3, 5) #trajectories 1, 3, 4, 6
     assert_equal(output, expected)
 
 
@@ -83,17 +94,70 @@ def test_scores_from_find_most_distant():
     np.testing.assert_allclose(output, expected, rtol=1e-1, atol=1e-2)
 
 
+def test_find_optimum_trajectories():
+    input_1 = [[0, 1/3.], [0, 1.], [2/3., 1.]]
+    input_2 = [[0, 1/3.], [2/3., 1/3.], [2/3., 1.]]
+    input_3 = [[2/3., 0], [2/3., 2/3.], [0, 2/3.]]
+    input_4 = [[1/3., 1.], [1., 1.], [1, 1/3.]]
+    input_5 = [[1/3., 1.], [1/3., 1/3.], [1, 1/3.]]
+    input_6 = [[1/3., 2/3.], [1/3., 0], [1., 0]]
+    input_sample = np.concatenate([input_1, input_2, input_3, input_4, input_5,
+                          input_6])
+    N = 6
+    num_params = 2
+    k_choices = 4
+    output = find_optimimum_trajectories(input_sample, N, num_params, k_choices)
+    expected = np.concatenate([input_1, input_3, input_4, input_6])
+    np.testing.assert_equal(output, expected)
+
+
+def test_readfile():
+
+    filename = "tests/test_params.txt"
+    pf = read_param_file(filename)
+
+    assert_equal(pf['bounds'], [[0, 100], [5, 51]])
+    assert_equal(pf['num_vars'], 2)
+    assert_equal(pf['names'], ['Test1', 'Test2'])
+
+
+#def test_scalefile():
+#    pass
+
+def test_morris_sample():
+
+    param_file = 'tests/test_params.txt'
+    pf = read_param_file(param_file)
+    num_params = pf['num_vars']
+    bounds = pf['bounds']
+    N = 500
+    p_levels = 4
+    grid_step = 2
+
+    a =  morris_sample(N,
+                       num_params,
+                       bounds,
+                       num_levels=p_levels,
+                       grid_jump=grid_step
+                       )
+
+    b = sample(N,
+               param_file,
+               num_levels=p_levels,
+               grid_jump=grid_step)
+
+    np.testing.assert_allclose(np.average(a),np.average(b),rtol=10e-02,atol=10e-02)
 #def test_Map():
 #    '''
 #
 #    '''
-#    sample_inputs = setUp()
-#    N = 6
-#    num_params = 2
-#    k_choices = 4
-#    distance_matrix = np.array(compute_distance_matrix(sample_inputs,
-#                                                    N,
-#                                                    num_params), dtype=np.float32)
+    #sample_inputs = setUp()
+    #N = 6
+    #num_params = 2
+    #k_choices = 4
+    #distance_matrix = np.array(compute_distance_matrix(sample_inputs,
+    #                                                N,
+    #                                                num_params), dtype=np.float32)
 #
 #    combos = [t for t in combinations(range(N), k_choices)]
 #    output = np.zeros(15, dtype=np.float32)
