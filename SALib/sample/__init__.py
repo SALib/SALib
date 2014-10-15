39b432f12c3d1586b61ebbbac92f0e7f4d1fa286
zoidy <test118@hotmail.com>
2014-10-15 11:27:25 -0400

    Revert "fix some __init__.py's to make it work"
    This reverts commit a84730bbdc9af503e47fc1a67c91e8caa145479a.

--- SALib/sample/__init__.py
+++
@@ -1,7 +0,0 @@
-import morris_oat
-import sobol_sequence
-import finite_diff
-import saltelli
-import latin
-import fast_sampler
-__all__ = ["latin", "saltelli", "sobol_sequence", "morris_oat", "fast_sampler","finite_diff"]
