e92e16bae33e4e2f9eeff016eb99747a686028af
Will Usher <w.usher@ucl.ac.uk>
2014-11-18 17:31:15 +0000

    Added tests for Sobol_G

--- SALib/test_functions/Sobol_G.py
+++ SALib/test_functions/Sobol_G.py
@@ -10,14 +10,25 @@ import numpy as np
 # x5-x8: 0.0001
 
 
-def evaluate(values):
-    a = [0, 1, 4.5, 9, 99, 99, 99, 99]
+def evaluate(values,a=[0, 1, 4.5, 9, 99, 99, 99, 99]):
+
+    if type(values) != np.ndarray:
+        raise TypeError("The argument `values` must be a numpy ndarray")
+
+    ltz = np.array(values) < 0
+    gtz = np.array(values) > 0
+
+    if ltz.any() == True:
+        raise ValueError("Sobol G function called with values less than one")
+    elif gtz.any() == True:
+        raise ValueError("Sobol G function called with values greater than one")
+
     Y = np.empty([values.shape[0]])
 
     for i, row in enumerate(values):
         Y[i] = 1.0
 
-        for j in range(8):
+        for j in range(len(a)):
             x = row[j]
             Y[i] *= (abs(4 * x - 2) + a[j]) / (1 + a[j])
 
---
+++ SALib/tests/test_test_functions.py
@@ -0,0 +1,51 @@
+from nose.tools import assert_almost_equal, assert_equal, raises
+from ..test_functions.Sobol_G import evaluate
+import numpy as np
+
+
+def test_Sobol_G():
+    desired = np.array([4.0583])
+
+    parameter_values = np.zeros((1,8))
+
+    output = evaluate(parameter_values)
+
+    np.testing.assert_almost_equal(output, desired, decimal=4)
+
+
+@raises(ValueError)
+def test_Sobol_G_raises_error_if_values_wrong_size():
+    """
+    Tests that a value error is raised if the Sobol G function is called with
+    the wrong number of variables
+    """
+    evaluate(np.array([1, 2, 3, 4, 5, 6, 7]))
+
+
+@raises(ValueError)
+def test_Sobol_G_raises_error_if_values_gt_one():
+    """
+    Tests that a value error is raised if the Sobol G function is called with
+    values greater than one
+    """
+    evaluate(np.array([0, 1, .02, 23, 39, 01, 0, 12]))
+
+
+@raises(ValueError)
+def test_Sobol_G_raises_error_if_values_lt_zero():
+    """
+    Tests that a value error is raised if the Sobol G function is called with
+    values less than zero.
+    """
+    evaluate(np.array([0, -1, -.02, 1, 1, -01, -0, -12]))
+
+
+@raises(TypeError)
+def test_Sobol_G_raises_error_if_values_not_numpy_array():
+    """
+    Tests that a type error is raised if the Sobol G function is called with
+    values argument not as a numpy array.
+    """
+    fixture = [list(range(8)), str(01234567)]
+    for x in fixture:
+        evaluate(x)
