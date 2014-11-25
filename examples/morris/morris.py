c4550eef19c50697a0b42eb9b14fd81e37a48a8b
Will Usher <w.usher@ucl.ac.uk>
2014-11-25 15:57:26 +0000

    Updated Python example to include calls to Morris class

--- examples/morris/morris.py
+++ examples/morris/morris.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../..')
 
-from SALib.sample import morris_oat
+from SALib.sample.morris import Morris
 from SALib.analyze import morris
 from SALib.test_functions import Ishigami
 import numpy as np
@@ -10,14 +10,20 @@ import numpy as np
 param_file = '../../SALib/test_functions/params/Ishigami.txt'
 
 # Generate samples
-param_values = morris_oat.sample(1000, param_file, num_levels=10, grid_jump=5)
+param_values = Morris(param_file, samples=1000, num_levels=10, grid_jump=5, \
+                      group=None, \
+                      optimal_trajectories=None)
 
 # Save the parameter values in a file (they are needed in the analysis)
-np.savetxt('model_input.txt', param_values, delimiter=' ')
+param_values.save_data('model_input.txt')
+
+param_values.debug()
 
 # Run the "model" and save the output in a text file
 # This will happen offline for external models
-Y = Ishigami.evaluate(param_values)
+
+online_model_values = param_values.get_input_sample_scaled()
+Y = Ishigami.evaluate(online_model_values)
 np.savetxt("model_output.txt", Y, delimiter=' ')
 
 # Perform the sensitivity analysis using the model output
@@ -27,3 +33,4 @@ Si = morris.analyze(param_file, 'model_input.txt', 'model_output.txt',
 # Returns a dictionary with keys 'mu', 'mu_star', 'sigma', and 'mu_star_conf'
 # e.g. Si['mu_star'] contains the mu* value for each parameter, in the
 # same order as the parameter file
+print(Si)
