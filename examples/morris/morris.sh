35f957b8801dd190099d41b3aab82b75420670e7
Will Usher <w.usher@ucl.ac.uk>
2014-11-25 16:07:52 +0000

    Updated shell example for morris to use Morris class

--- examples/morris/morris.sh
+++ examples/morris/morris.sh
@@ -2,35 +2,42 @@
 
 # Example: generating samples from the command line
 cd ../../ # hack
-python -m SALib.sample.morris_oat \
+python -m SALib.sample.morris \
 	   -n 100 \
 	   -p ./SALib/test_functions/params/Ishigami.txt \
 	   -o model_input.txt \
 	   --delimiter=' ' \
 	   --precision=8 \
-        --num-levels=10 \
-        --grid-jump=5
+     -l=10 \
+     --grid-jump=5
 
 # Options:
-# -p, --paramfile: Your parameter range file (3 columns: parameter name, lower bound, upper bound)
+# -p, --paramfile: Your parameter range file
+#                  (3 columns: parameter name,
+#                              lower bound,
+#                              upper bound)
 #
-# -n, --samples: Sample size. 
+# -n, --samples: Sample size.
 #				 Number of model runs is N(D + 1)
 #
 # -o, --output: File to output your samples into.
-# 
+#
 # --delimiter (optional): Output file delimiter.
 #
 # --precision (optional): Digits of precision in the output file. Default is 8.
 #
-# --morris-num-levels (optional): Number of levels in the OAT sampling. The range of each variable will be discretized into this many levels. Default is 10.
+# -l (optional): Number of levels in the OAT sampling.
+#                The range of each variable will be discretized into this many levels.
+#                Default is 10.
 #
-# --morris-grid-jump (optional): Grid jump size in the OAT sampling. Each variable will be perturbed by this number of levels during each trajectory. Default is 5.
+# --grid-jump (optional): Grid jump size in the OAT sampling.
+#                         Each variable will be perturbed by this number of levels
+#                         during each trajectory. Default is 5.
 
 # Run the model using the inputs sampled above, and save outputs
 python -c "from SALib.test_functions import Ishigami; import numpy as np; np.savetxt('model_output.txt', Ishigami.evaluate(np.loadtxt('model_input.txt')))"
 
-# Then use the output to run the analysis. 
+# Then use the output to run the analysis.
 # Sensitivity indices will print to command line. Use ">" to write to file.
 
 python -m SALib.analyze.morris \
@@ -41,15 +48,19 @@ python -m SALib.analyze.morris \
      -r 1000
 
 # Options:
-# -p, --paramfile: Your parameter range file (3 columns: parameter name, lower bound, upper bound)
+# -p, --paramfile: Your parameter range file
+#                  (3 columns: parameter name,
+#                              lower bound,
+#                              upper bound)
 #
 # -Y, --model-output-file: File of model output values to analyze
 #
-# -c, --column (optional): Column of model output file to analyze. 
+# -c, --column (optional): Column of model output file to analyze.
 #                If the file only has one column, this argument will be ignored.
 #
 # --delimiter (optional): Model output file delimiter.
 #
 # -X, --model-input-file: File of model input values (parameter samples).
 #
-# -r, --resamples (optional): Number of bootstrap resamples used to calculate confidence intervals on indices. Default 1000.
+# -r, --resamples (optional): Number of bootstrap resamples used to calculate confidence
+#                             intervals on indices. Default 1000.
