7fbcf1bc94ccc0fc0fe0803efe2fd5aa80b43486
Will Usher <w.usher@ucl.ac.uk>
2014-11-28 16:16:06 +0000

    Added auto-version numbering to repository

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'SALib/_version.py'
+versioneer.versionfile_build = None
+versioneer.tag_prefix = 'v' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'SALib-' # dirname like 'myproject-1.2.0'
 
 class NoseTestCommand(TestCommand):
 
@@ -15,40 +20,52 @@ class NoseTestCommand(TestCommand):
         nose.run_exit(argv=['nosetests'])
 
 
-setup(
-    name='SALib',
-    version="0.1",
-    packages=find_packages(exclude=["*tests*"]),
-    author="Jon Herman",
-    author_email="jdherman8@gmail.com",
-    license=open('LICENSE.md').read(),
-    tests_require=['nose'],
-    cmdclass={'test': NoseTestCommand},
-    install_requires=[
-        "numpy",
-        "scipy",
-        "scikit-learn",
-    ],
-    url="https://github.com/jdherman/SALib",
-    long_description=open('README.md').read(),
-    description=(
-        'Tools for sensitivity analysis. Contains Sobol, Morris, and FAST methods.'),
-    # entry_points = {
-    #     'console_scripts': [
-    #         'salib = SALib.bin.salib:main',
-    #     ]
-    # },
-    classifiers=[
-        # 'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        # 'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 2 :: Only',
-        'Topic :: Scientific/Engineering :: Mathematics',
-    ],)
+
+
+def setup_package():
+    # Assemble additional setup commands
+    cmdclass = versioneer.get_cmdclass()
+    cmdclass['test'] = NoseTestCommand
+
+    setup(
+        name='SALib',
+        packages=find_packages(exclude=["*tests*"]),
+        author="Jon Herman",
+        author_email="jdherman8@gmail.com",
+        license=open('LICENSE.md').read(),
+        tests_require=['nose'],
+        install_requires=[
+            "numpy",
+            "scipy",
+            "scikit-learn",
+        ],
+        # Two arguments required by Versioneer
+        version = versioneer.get_version(),
+        cmdclass=cmdclass,
+        url="https://github.com/jdherman/SALib",
+        long_description=open('README.md').read(),
+        description=(
+            'Tools for sensitivity analysis. Contains Sobol, Morris, and FAST methods.'),
+        # entry_points = {
+        #     'console_scripts': [
+        #         'salib = SALib.bin.salib:main',
+        #     ]
+        # },
+        classifiers=[
+            # 'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            # 'License :: OSI Approved :: BSD License',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            # 'Programming Language :: Python :: 2.7',
+            # 'Programming Language :: Python :: 2 :: Only',
+            'Topic :: Scientific/Engineering :: Mathematics',
+        ],)
+
+
+if __name__ == "__main__":
+    setup_package()
