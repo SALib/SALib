f140d293c0850c66d774858461058f25f04a6576
Will Usher <w.usher@ucl.ac.uk>
2015-06-26 18:11:58 +0100

    Extended Sobol_G test function
    Added analytic computation of sensitivity index from Sobol G test function
    Tests for above
    Test for Sobol/Saltelli procedure which compares again analytic computation
    of sensitivity index from sobol G test function

--- SALib/test_functions/Sobol_G.py
+++ SALib/test_functions/Sobol_G.py
@@ -16,11 +16,11 @@ def evaluate(values, a=[0, 1, 4.5, 9, 99, 99, 99, 99]):
         raise TypeError("The argument `values` must be a numpy ndarray")
 
     ltz = np.array(values) < 0
-    gtz = np.array(values) > 0
+    gto = np.array(values) > 1
 
     if ltz.any() == True:
         raise ValueError("Sobol G function called with values less than one")
-    elif gtz.any() == True:
+    elif gto.any() == True:
         raise ValueError("Sobol G function called with values greater than one")
 
     Y = np.empty([values.shape[0]])
@@ -33,3 +33,29 @@ def evaluate(values, a=[0, 1, 4.5, 9, 99, 99, 99, 99]):
             Y[i] *= (abs(4 * x - 2) + a[j]) / (1 + a[j])
 
     return Y
+
+
+def partial_first_order_variance(a=[0, 1, 4.5, 9, 99, 99, 99, 99]):
+    a = np.array(a)
+    return np.divide(1, np.multiply(3, np.square(1 + a)))
+
+
+def total_variance(a=[0, 1, 4.5, 9, 99, 99, 99, 99]):
+    a = np.array(a)
+    return np.add(-1, np.product(1 + partial_first_order_variance(a), axis=0))
+
+
+def sensitivity_index(a):
+    a = np.array(a)
+    return np.divide(partial_first_order_variance(a), total_variance(a))
+
+
+def total_sensitivity_index(a):
+    a = np.array(a)
+    
+    pv = partial_first_order_variance(a)
+    tv = total_variance(a)
+    
+    sum_pv = pv.sum(axis=0)
+    
+    return np.subtract(1, np.divide(np.subtract(sum_pv, pv.T), tv))
--- SALib/tests/test_test_functions.py
+++ SALib/tests/test_test_functions.py
@@ -1,9 +1,12 @@
 from nose.tools import assert_almost_equal, assert_equal, raises
 
+from numpy.testing import assert_almost_equal
 import numpy as np
 
-from ..test_functions.Sobol_G import evaluate
-
+from ..test_functions.Sobol_G import evaluate, total_variance, \
+                                     partial_first_order_variance, \
+                                     sensitivity_index, \
+                                     total_sensitivity_index
 
 def test_Sobol_G():
     desired = np.array([4.0583])
@@ -12,7 +15,7 @@ def test_Sobol_G():
 
     output = evaluate(parameter_values)
 
-    np.testing.assert_almost_equal(output, desired, decimal=4)
+    assert_almost_equal(output, desired, decimal=4)
 
 
 @raises(ValueError)
@@ -51,3 +54,45 @@ def test_Sobol_G_raises_error_if_values_not_numpy_array():
     fixture = [list(range(8)), str(12345678)]
     for x in fixture:
         evaluate(x)
+
+
+def test_total_variance():
+    
+    a = np.array([78, 12, 0.5, 2, 97, 33])
+    actual = total_variance(a)
+    
+    assert_almost_equal(actual, 0.193470, decimal=4)    
+
+def test_partial_first_order_variance():
+    
+    a = np.array([78, 12, 0.5, 2, 97, 33])
+    actual = partial_first_order_variance(a)
+    expected = (len(a),)
+    
+    assert_equal(a.shape, expected)
+    
+    expected = np.array([0.000053, 0.001972, 0.148148, 0.037037, 0.000035, 0.000288])
+    
+    assert_almost_equal(actual, expected, decimal=4)
+
+def test_sensitivity_index():
+    a = np.array([78, 12, 0.5, 2, 97, 33])
+    actual = sensitivity_index(a)
+    expected = np.array([0.000276, 0.010195, 0.765743, 
+                         0.191436, 0.000179, 0.001490])
+    assert_almost_equal(actual, expected, decimal=4)
+    
+    
+def test_total_sensitivity_index():
+    a = np.array([78, 12, 0.5, 2, 97, 33])
+    
+    actual = total_sensitivity_index(a)
+    
+    expected = np.array([0.030956547, 0.040875287, 0.796423551, 
+                         0.222116249, 0.030859879, 0.032170899])
+    
+    assert_almost_equal(actual, expected, decimal=4)
+         
+                  
+                  
+                  
