fae4725136ec75171a66827dacca69968359579b
Will Usher <w.usher@ucl.ac.uk>
2015-06-26 18:20:01 +0100

    More changes to tests for Sobol_G test function

--- SALib/tests/test_test_functions.py
+++ SALib/tests/test_test_functions.py
@@ -1,6 +1,6 @@
 from nose.tools import assert_almost_equal, assert_equal, raises
 
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_allclose
 import numpy as np
 
 from ..test_functions.Sobol_G import evaluate, total_variance, \
@@ -9,13 +9,12 @@ from ..test_functions.Sobol_G import evaluate, total_variance, \
                                      total_sensitivity_index
 
 def test_Sobol_G():
-    desired = np.array([4.0583])
-
+    '''
+    '''
     parameter_values = np.zeros((1, 8))
-
-    output = evaluate(parameter_values)
-
-    assert_almost_equal(output, desired, decimal=4)
+    actual = evaluate(parameter_values)
+    expected = np.array([4.0583])
+    assert_allclose(actual, expected, atol=1e-4, rtol=1e-4)
 
 
 @raises(ValueError)
@@ -24,7 +23,8 @@ def test_Sobol_G_raises_error_if_values_wrong_size():
     Tests that a value error is raised if the Sobol G function is called with
     the wrong number of variables
     """
-    evaluate(np.array([1, 2, 3, 4, 5, 6, 7]))
+    a = [1, 2, 3, 4, 5, 6, 7, 8]
+    evaluate(np.array([1, 2, 3, 4, 5, 6, 7]), a)
 
 
 @raises(ValueError)
@@ -60,8 +60,10 @@ def test_total_variance():
     
     a = np.array([78, 12, 0.5, 2, 97, 33])
     actual = total_variance(a)
+    expected = 0.19347
     
-    assert_almost_equal(actual, 0.193470, decimal=4)    
+    assert_allclose(actual, expected, rtol=1e-4)    
+
 
 def test_partial_first_order_variance():
     
@@ -73,14 +75,15 @@ def test_partial_first_order_variance():
     
     expected = np.array([0.000053, 0.001972, 0.148148, 0.037037, 0.000035, 0.000288])
     
-    assert_almost_equal(actual, expected, decimal=4)
+    assert_allclose(actual, expected, atol=1e-4, rtol=1e-4)
+
 
 def test_sensitivity_index():
     a = np.array([78, 12, 0.5, 2, 97, 33])
     actual = sensitivity_index(a)
-    expected = np.array([0.000276, 0.010195, 0.765743, 
+    expected = np.array([0.000276, 0.010195, 0.765743,
                          0.191436, 0.000179, 0.001490])
-    assert_almost_equal(actual, expected, decimal=4)
+    assert_allclose(actual, expected, atol=1e-2, rtol=1e-6)
     
     
 def test_total_sensitivity_index():
@@ -88,11 +91,7 @@ def test_total_sensitivity_index():
     
     actual = total_sensitivity_index(a)
     
-    expected = np.array([0.030956547, 0.040875287, 0.796423551, 
+    expected = np.array([0.030956547, 0.040875287, 0.796423551,
                          0.222116249, 0.030859879, 0.032170899])
     
-    assert_almost_equal(actual, expected, decimal=4)
-         
-                  
-                  
-                  
+    assert_allclose(actual, expected, atol=1e-2, rtol=1e-6)             
