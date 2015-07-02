4d31bb585f9ab8e97b878a6b04d861e3dd3ca119
Will Usher <w.usher@ucl.ac.uk>
2015-07-02 17:38:38 +0100

    Refactored ff and added more tests for new procedures. Also amended calculation of main effect.

--- SALib/sample/ff.py
+++ SALib/sample/ff.py
@@ -11,12 +11,33 @@ from ..util import scale_samples, read_param_file
 
 
 def find_smallest(num_vars):
+    '''
+    Find the smallest exponent of two that is greater than the number
+    of variables
+    '''
     for x in range(10):
         if num_vars <= 2 ** x:
             return x
 
 
-def sample(problem):
+def extend_bounds(problem):
+    
+    num_vars = problem['num_vars']
+    num_ff_vars = 2 ** find_smallest(num_vars)
+    num_dummy_variables = num_ff_vars - num_vars
+    
+    bounds = list(problem['bounds'])
+    names = problem['names']
+    if num_dummy_variables > 0:
+        bounds.extend([[0, 1] for x in range(num_dummy_variables)])
+        names.extend(["dummy_" + str(var) for var in range(num_dummy_variables)])
+        problem['bounds'] = bounds
+        problem['names'] = names
+        problem['num_vars'] = num_ff_vars
+    
+    return problem
+
+def generate_contrast(problem):
     
     num_vars = problem['num_vars']
     
@@ -24,21 +45,18 @@ def sample(problem):
     k = [2 ** n for n in range(16)]
     k_chosen = 2 ** find_smallest(num_vars)
     
-    num_dummy_variables = k_chosen - num_vars
-
-    # Generate the sample
-    sample = np.vstack([hadamard(k_chosen), -hadamard(k_chosen)])
+    # Generate the fractional factorial contrast
+    contrast = np.vstack([hadamard(k_chosen), -hadamard(k_chosen)])
     
-    sample = np.array((sample + 1.) / 2, dtype=np.float)
+    return contrast
     
-    bounds = list(problem['bounds'])
-    if num_dummy_variables > 0:
-        bounds.extend([[0, 1] for x in range(num_dummy_variables)])
+def sample(problem):
     
-    scale_samples(sample, bounds)
-    
+    contrast = generate_contrast(problem)
+    sample = np.array((contrast + 1.) / 2, dtype=np.float)
+    problem = extend_bounds(problem)
+    scale_samples(sample, problem['bounds'])
     return sample
-    
 
 if __name__ == "__main__":
 
--- SALib/tests/test_ff.py
+++ SALib/tests/test_ff.py
@@ -3,11 +3,41 @@ Created on 30 Jun 2015
 
 @author: will2
 '''
-from numpy.testing import assert_equal
-from SALib.sample.ff import sample
 import numpy as np
+from numpy.testing import assert_equal, assert_allclose
+from ..sample.ff import sample, find_smallest, extend_bounds
+from ..analyze.ff import analyze, interactions
 
-from SALib.analyze.ff import analyze
+
+def test_find_smallest():
+    '''
+    '''
+    num_vars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 31, 32, 33]
+    expected = [0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6]
+    for x, y in zip(num_vars, expected):
+        actual = find_smallest(x)
+        assert_equal(actual, y)
+
+
+def test_extend_bounds():
+    problem = {'bounds': np.repeat([-1, 1], 12).reshape(2, 12).T,
+           'num_vars': 12,
+           'names': ["x" + str(x + 1) for x in range(12)]
+           }
+    actual = extend_bounds(problem)
+    expected = {'names': ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'dummy_0', 'dummy_1', 'dummy_2', 'dummy_3'], 
+                'bounds': [np.array([-1,  1]), np.array([-1,  1]), 
+                           np.array([-1,  1]), np.array([-1,  1]), 
+                           np.array([-1,  1]), np.array([-1,  1]), 
+                           np.array([-1,  1]), np.array([-1,  1]), 
+                           np.array([-1,  1]), np.array([-1,  1]), 
+                           np.array([-1,  1]), np.array([-1,  1]), 
+                           np.array([0, 1]), np.array([0, 1]), 
+                           np.array([0, 1]), np.array([0, 1])], 
+                'num_vars': 16}
+
+    print actual
+    assert_equal(actual, expected)
 
 def test_ff_sample():
     problem = {'bounds': [[0., 1.], [0., 1.], [0., 1.], [0., 1.]],
@@ -60,7 +90,7 @@ def test_ff_analyze():
                   [0, 1, 1, 0]], dtype=np.float)
     Y = np.array([1.5, 1, 1.5, 1, 2, 2.5, 2, 2.5], dtype=np.float)
     actual = analyze(problem, X, Y)
-    expected = {'ME': np.array([ 0.625, 1.   , 0.875, 0.875]), 'names': ['x1', 'x2', 'x3', 'x4']}
+    expected = {'ME': np.array([ -0.5 ,  0.25,  0.  ,  0.  ]), 'names': ['x1', 'x2', 'x3', 'x4']}
     assert_equal(actual, expected)
 
 
@@ -89,20 +119,55 @@ def test_ff_example():
     expected = np.array([1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float)
     assert_equal(expected, Si['ME'])
     
+
+def test_interactions_from_saltelli():
+    '''
+    '''
+    problem = {'bounds': np.repeat([-1, 1], 12).reshape(2, 12).T,
+               'num_vars': 12,
+               'names': ["x" + str(x + 1) for x in range(12)]
+               }
+    
+    X = sample(problem)
+
+    Y = np.array([10, -2, 4, -8, 2, 6, -4, 0,
+                   2, 6, -4, 0, 10, -2, 4, -8,
+                  - 2, -6, 4, 0, -10, 2, -4, 8,
+                 - 10, 2, -4, 8, -2, -6, 4, 0])
+    
+    Si = analyze(problem, X, Y, second_order=True)
+    print Si
+    actual = Si['IE']
+    expected = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    
+    assert_equal(actual, expected)
+    
+    
 def test_interactions():
     '''
     '''
     problem = {'bounds': [[0., 2.5], [0., 1.], [0., 1.], [0., 1.]],
                'num_vars': 4,
                'names': ['x1', 'x2', 'x3', 'x4']}
-    X = np.array([[ 1, 1, 1, 1],
-                  [ 1, 0, 1, 0],
-                  [ 1, 1, 0, 0],
-                  [ 1, 0, 0, 1],
+    X = np.array([[ 2.5, 1.0, 1.0, 1.0],
+                  [ 2.5, 0, 1.0, 0],
+                  [ 2.5, 1.0, 0, 0],
+                  [ 2.5, 0, 0, 1.0],
                   [0, 0, 0, 0],
-                  [0, 1, 0, 1],
-                  [0, 0, 1, 1],
-                  [0, 1, 1, 0]], dtype=np.float)
-    Y = np.array([1.5, 1, 1.5, 1, 2, 2.5, 2, 2.5], dtype=np.float)
-    actual = analyze(problem, X, Y, second_order=True, print_to_console=True)
-    assert_equal(1,0)
+                  [0, 1.0, 0, 1.0],
+                  [0, 0, 1.0, 1.0],
+                  [0, 1.0, 1.0, 0]], dtype=np.float)
+    Y = X[:, 0] + (0.1 * X[:, 1]) + ((1.2 * X[:, 2]) * (1.3 + X[:, 3]))
+#     Y = np.array([1.5, 1, 1.5, 1, 2, 2.5, 2, 2.5], dtype=np.float)
+    ie_names, ie = interactions(problem, Y, print_to_console=True)
+    actual = ie
+    assert_allclose(actual, [0.3, 0, 0, 0, 0, 0.3], rtol=1e-4, atol=1e-4)
