2a1890c19dd957432a8a9b0e6d6ad1bfbfba6ce8
Will Usher <w.usher@ucl.ac.uk>
2015-07-07 12:12:13 +0100

    Fixed Py3 print

--- SALib/analyze/ff.py
+++ SALib/analyze/ff.py
@@ -4,6 +4,7 @@ Created on 30 Jun 2015
 @author: will2
 '''
 
+from __future__ import print_function
 import numpy as np
 from . import common_args
 from ..util import read_param_file, unscale_samples
@@ -60,7 +61,7 @@ def interactions(problem, Y, print_to_console=False):
             ie_names.append(var_names)
             IE.append((1. / (2 * num_vars)) * np.dot(Y, x))
     if print_to_console:
-        print[('%s %f \n' % (n, i) ) for (n, i) in zip(ie_names, IE) ]
+        [print('%s %f' % (n, i) ) for (n, i) in zip(ie_names, IE) ]
     
     return ie_names, IE
 
