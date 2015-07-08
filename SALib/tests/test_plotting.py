6f8c9c954ebc53765edfa5c49e52611826462bc8
Will Usher <w.usher@ucl.ac.uk>
2015-07-08 16:28:20 +0100

    Updated plotting tests

--- SALib/tests/test_plotting.py
+++ SALib/tests/test_plotting.py
@@ -7,13 +7,14 @@ A useful guide as to how to get image_comparison working on your local machine
 http://www.davidketcheson.info/2015/01/13/using_matplotlib_image_comparison.html
 '''
 import numpy as np
-import matplotlib
 from matplotlib.testing.decorators import image_comparison
 import matplotlib.pyplot as plt
 
-from ..SALib.plotting import morris
+from SALib.plotting.morris import horizontal_bar_plot, \
+                                  covariance_plot, \
+                                  sample_histograms
 
-@image_comparison(baseline_images=['morris_horiz_bar_plot'])
+@image_comparison(baseline_images=['horizontal_bar_plot'])
 def test_morris_horizontal_bar_plot():
     # Create a fixture to represent Si, the dictionary of output metrics
     
@@ -21,5 +22,30 @@ def test_morris_horizontal_bar_plot():
           'names':['x1', 'x2', 'x3', 'x4', 'x5', 'x6'],
           'mu_star_conf':[0.5, 1, 1.5, 2, 2.5, 3.0]} 
     
-    morris.horizontal_bar_plot(Si, sortby='mu_star')
+    horizontal_bar_plot(Si, sortby='mu_star')
 #     plt.savefig('morris_horiz_bar_plot.png')
+
+
+@image_comparison(baseline_images=['covariance_plot'])
+def test_morris_covariance_plot():
+    # Create a fixture to represent Si, the dictionary of output metrics
+    Si = {'mu_star':[1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+          'names':['x1', 'x2', 'x3', 'x4', 'x5', 'x6'],
+          'mu_star_conf':[0.5, 1, 1.5, 2, 2.5, 3.0]} 
+    covariance_plot(Si, unit="")
+
+
+@image_comparison(baseline_images=['sample_histograms'])
+def test_morris_sample_histograms():
+    
+    input_1 = [[0, 1 / 3.], [0, 1.], [2 / 3., 1.]]
+    input_2 = [[0, 1 / 3.], [2 / 3., 1 / 3.], [2 / 3., 1.]]
+    input_3 = [[2 / 3., 0], [2 / 3., 2 / 3.], [0, 2 / 3.]]
+    input_4 = [[1 / 3., 1.], [1., 1.], [1, 1 / 3.]]
+    input_5 = [[1 / 3., 1.], [1 / 3., 1 / 3.], [1, 1 / 3.]]
+    input_6 = [[1 / 3., 2 / 3.], [1 / 3., 0], [1., 0]]
+    input_sample = np.concatenate([input_1, input_2, input_3,
+                                   input_4, input_5, input_6])
+    problem = {'num_vars': 2, 'groups': None}
+    
+    sample_histograms(input_sample, problem)
