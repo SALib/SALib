56fe4b48aac9e99b568d0b9cb9a48801b02150e6
bernardoct <bct52@cornell.edu>
2015-08-28 12:16:49 -0400

    Update sobol.py
    New async parallel

--- SALib/analyze/sobol.py
+++ SALib/analyze/sobol.py
@@ -4,22 +4,28 @@ from __future__ import print_function
 from scipy.stats import norm
 
 import numpy as np
+import random as rd
 
 from . import common_args
 from ..util import read_param_file
 
 from multiprocessing import Pool, cpu_count
 from functools import partial
+try:
+    from itertools import zip_longest
+except ImportError:
+    # Python 2
+    from itertools import izip_longest as zip_longest
 
-EMPTY = -1
 
-# Perform Sobol Analysis on file of model results
+# Perform Sobol Analysis on a vector of model results
 # Returns a dictionary with keys 'S1', 'S1_conf', 'ST', and 'ST_conf'
 # Where each entry is a list of size D (the number of parameters)
 # Containing the indices in the same order as the parameter file
 def analyze(problem, Y, calc_second_order=True, num_resamples=100,
-            conf_level=0.95, print_to_console=False, parallel=1,
-            n_processors=1):
+            conf_level=0.95, print_to_console=False, parallel=False,
+            n_processors=None):
+
     D = problem['num_vars']
 
     if calc_second_order and Y.size % (2 * D + 2) == 0:
@@ -34,136 +40,45 @@ def analyze(problem, Y, calc_second_order=True, num_resamples=100,
     if conf_level < 0 or conf_level > 1:
         raise RuntimeError("Confidence level must be between 0-1.")
 
-    A = np.empty(N)
-    B = np.empty(N)
-    AB = np.empty((N, D))
-    BA = np.empty((N, D)) if calc_second_order else None
-    step = 2 * D + 2 if calc_second_order else D + 2
-
-    A = Y[0:Y.size:step]
-    B = Y[(step - 1):Y.size:step]
-    for j in range(D):
-        AB[:, j] = Y[(j + 1):Y.size:step]
-        if calc_second_order:
-            BA[:, j] = Y[(j + 1 + D):Y.size:step]
-
-    # First order (+conf.) and Total order (+conf.)
-    keys = ('S1', 'S1_conf', 'ST', 'ST_conf')
-    S = dict((k, np.empty(D)) for k in keys)
-
-    if calc_second_order:
-        S['S2'] = np.empty((D, D))
-        S['S2'][:] = np.nan
-        S['S2_conf'] = np.empty((D, D))
-        S['S2_conf'][:] = np.nan
-
+    A,B,AB,BA = separate_output_values(Y, D, N, calc_second_order)
     r = np.random.randint(N, size=(N, num_resamples))
     Z = norm.ppf(0.5 + conf_level / 2)
 
-    # Define number of procesors to be used if parallel option is selected
-    if parallel > 0 and n_processors == 1:
-        n_processors = min(
-            cpu_count() if parallel == 2 else int(cpu_count() / 2 + 0.5),
-            D * len(keys))
-
-    # Create list with one entry (key, parameter 1, parameter 2) per sobol
-    # index (+conf.).
-    entries = [[d, j, EMPTY] for j in range(D) for d in keys]
-    pool_entries = list(np.array_split(entries, n_processors))
-
-    # Add second order (+conf.) to entries
-    if calc_second_order:
-        entries_second_order = [[d, j, k] for j in range(D) for k in
-                                  range(j + 1, D) for d in ('S2', 'S2_conf')]
-        # Split list of entries into one sublist per processor to be used
-        pool_entries_second_order = list(
-            np.array_split(entries_second_order, n_processors))
-
-        # Include second order terms in the list of entries. They are spread
-        # across the processes for better efficiency, as second order terms are
-        # more computationaly intensive.
-        pool_entries_dummy = []
-        for p1o, p2o in zip(reversed(pool_entries),
-                            pool_entries_second_order):
-            pool_entries_dummy.append(list(p1o) + list(p2o))
-
-        pool_entries = pool_entries_dummy
-
-    # Calculate Sobol indexes
-    if n_processors > 1:  # Parallelized calculation
-        func = partial(sobol_indexes_calculation, Z, A, AB, BA, B, r)
-        pool = Pool(n_processors)
-        sobol_indexes_raw = pool.map(func, pool_entries)
-    else:  # Serial calculation
-        sobol_indexes_raw = []
-        for S_entry in pool_entries:
-            sobol_indexes_raw.append(
-                sobol_indexes_calculation(Z, A, AB, BA, B, r, S_entry))
-
-    # Populate results dictionary
-    sobol_indexes_list = []
-    for sir in sobol_indexes_raw:
-        sobol_indexes_list += sir
-
-    for si in sobol_indexes_list:  # First order (+conf.)
-        if si[2] == str(EMPTY):
-            S[si[0]][int(si[1])] = si[3]
-
-    if calc_second_order:  # Second order (+conf.)
-        for si in sobol_indexes_list:
-            if si[2] != str(EMPTY):
-                S[si[0]][int(si[1]), int(si[2])] = si[3]
-
-    # Print results to console
-    if print_to_console:
-
-        # First order (+conf.)
-        print("Parameter %s %s %s %s" % keys)
+    if not parallel:
+        S = create_Si_dict(D, calc_second_order)
 
         for j in range(D):
-            print("%s %f %f %f %f" % (problem['names'][j], S['S1'][
-                j], S['S1_conf'][j], S['ST'][j], S['ST_conf'][j]))
+            S['S1'][j] = first_order(A, AB[:, j], B)
+            S['S1_conf'][j] = Z * first_order(A[r], AB[r,j], B[r]).std(ddof=1)
+            S['ST'][j] = total_order(A, AB[:, j], B)
+            S['ST_conf'][j] = Z * total_order(A[r], AB[r,j], B[r]).std(ddof=1)
 
         # Second order (+conf.)
         if calc_second_order:
-            if print_to_console:
-                print("\nParameter_1 Parameter_2 S2 S2_conf")
-
             for j in range(D):
                 for k in range(j + 1, D):
-                    if print_to_console:
-                        print("%s %s %f %f" % (problem['names'][j], problem[
-                            'names'][k], S['S2'][j, k], S['S2_conf'][j, k]))
+                    S['S2'][j, k] = second_order(
+                        A, AB[:, j], AB[:, k], BA[:, j], B)
+                    S['S2_conf'][j, k] = Z * second_order(A[r], AB[r, j], 
+                        AB[r, k], BA[r, j], B[r]).std(ddof=1)
+
+    else:            
+        tasks = create_task_list(D, calc_second_order, n_processors)
+
+        func = partial(sobol_parallel, Z, A, AB, BA, B, r)
+        pool = Pool(n_processors)
+        S_list = pool.map_async(func, tasks)
+        pool.close()
+        pool.join()
+
+        S = Si_list_to_dict(S_list.get(), D, calc_second_order)
+
+    # Print results to console
+    if print_to_console:
+        print_indices(S, problem, calc_second_order)
+
 
     return S
-
-
-def sobol_indexes_calculation(Z, A, AB, BA, B, r, entries):
-    sobol_indexes = []
-    for S_entry in entries:
-        d = S_entry[0]
-        j = S_entry[1]
-        k = S_entry[2]
-
-        if d == 'S1':
-            sobol_indexes.append([d, j, k, first_order(A, AB[:, j], B)])
-        elif d == 'S1_conf':
-            sobol_indexes.append(
-                [d, j, k, Z * first_order(A[r], AB[r, j], B[r]).std(ddof=1)])
-        elif d == 'ST':
-            sobol_indexes.append([d, j, k, total_order(A, AB[:, j], B)])
-        elif d == 'ST_conf':
-            sobol_indexes.append(
-                [d, j, k, Z * total_order(A[r], AB[r, j], B[r]).std(ddof=1)])
-        if d == 'S2':
-            sobol_indexes.append(
-                [d, j, k, second_order(A, AB[:, j], AB[:, k], BA[:, j], B)])
-        elif d == 'S2_conf':
-            sobol_indexes.append([d, j, k,
-                                  second_order(A[r], AB[r, j], AB[r, k],
-                                               BA[r, j], B[r]).std(ddof=1)])
-
-    return sobol_indexes
 
 
 def first_order(A, AB, B):
@@ -185,6 +100,114 @@ def second_order(A, ABj, ABk, BAj, B):
     Sk = first_order(A, ABk, B)
 
     return Vjk - Sj - Sk
+
+
+def create_Si_dict(D, calc_second_order):
+    # initialize empty dict to store sensitivity indices
+    S = dict((k, np.empty(D)) for k in ('S1', 'S1_conf', 'ST', 'ST_conf'))
+
+    if calc_second_order:
+        S['S2'] = np.empty((D, D))
+        S['S2'][:] = np.nan
+        S['S2_conf'] = np.empty((D, D))
+        S['S2_conf'][:] = np.nan
+
+    return S
+
+
+def separate_output_values(Y, D, N, calc_second_order): 
+    AB = np.empty((N, D))
+    BA = np.empty((N, D)) if calc_second_order else None
+    step = 2 * D + 2 if calc_second_order else D + 2
+
+    A = Y[0:Y.size:step]
+    B = Y[(step - 1):Y.size:step]
+    for j in range(D):
+        AB[:, j] = Y[(j + 1):Y.size:step]
+        if calc_second_order:
+            BA[:, j] = Y[(j + 1 + D):Y.size:step]
+
+    return A,B,AB,BA
+
+
+def sobol_parallel(Z, A, AB, BA, B, r, tasks):
+    sobol_indices = []
+    for d,j,k in tasks:
+        if d == 'S1':
+            s = first_order(A, AB[:, j], B)
+        elif d == 'S1_conf':
+            s = Z * first_order(A[r], AB[r, j], B[r]).std(ddof=1)
+        elif d == 'ST':
+            s = total_order(A, AB[:, j], B)
+        elif d == 'ST_conf':
+            s = Z * total_order(A[r], AB[r, j], B[r]).std(ddof=1)
+        if d == 'S2':
+            s = second_order(A, AB[:, j], AB[:, k], BA[:, j], B)
+        elif d == 'S2_conf':
+            s = Z * second_order(A[r], AB[r, j], AB[r, k],
+                                               BA[r, j], B[r]).std(ddof=1)
+        sobol_indices.append([d, j, k, s])
+
+    return sobol_indices
+
+
+def create_task_list(D, calc_second_order, n_processors):
+    # Create list with one entry (key, parameter 1, parameter 2) per sobol
+    # index (+conf.). This is used to supply parallel tasks to multiprocessing.Pool
+    tasks_first_order = [[d, j, None] for j in range(D) for d in ('S1', 'S1_conf', 'ST', 'ST_conf')]
+    
+    # Add second order (+conf.) to tasks
+    tasks_second_order = []
+    if calc_second_order:
+        tasks_second_order = [[d, j, k] for j in range(D) for k in
+                            range(j + 1, D) for d in ('S2', 'S2_conf')]
+
+    if n_processors == None:
+        n_processors = min(cpu_count(), len(tasks_first_order) + len(tasks_second_order))
+
+    if not calc_second_order:
+        tasks = np.array_split(tasks_first_order, n_processors)
+    else:
+        # merges both lists alternating its elements and splits the resulting list into n_processors sublists
+        tasks = np.array_split([v for v in sum(
+            zip_longest(tasks_first_order[::-1], tasks_second_order), ()) 
+                if v is not None], n_processors)
+
+    return tasks
+
+
+def Si_list_to_dict(S_list, D, calc_second_order):
+    # Convert the parallel output into the regular dict format for printing/returning
+    S = create_Si_dict(D, calc_second_order)
+    L = []
+    for l in S_list: # first reformat to flatten
+        L += l
+
+    for s in L:  # First order (+conf.)
+        if s[2] is None:
+            S[s[0]][s[1]] = s[3]
+        else:
+            S[s[0]][s[1], s[2]] = s[3]
+
+    return S
+
+
+def print_indices(S, problem, calc_second_order):
+    # Output to console
+    D = problem['num_vars']
+    print('Parameter S1 S1_conf ST ST_conf')
+
+    for j in range(D):
+        print('%s %f %f %f %f' % (problem['names'][j], S['S1'][
+            j], S['S1_conf'][j], S['ST'][j], S['ST_conf'][j]))
+
+    if calc_second_order:
+        print('\nParameter_1 Parameter_2 S2 S2_conf')
+
+        for j in range(D):
+            for k in range(j + 1, D):
+                print("%s %s %f %f" % (problem['names'][j], problem[
+                    'names'][k], S['S2'][j, k], S['S2_conf'][j, k]))
 
 
 if __name__ == "__main__":
