1b79cc09b7543da4888dae13a43840935f8db2bd
Will Usher <w.usher@ucl.ac.uk>
2015-09-23 14:27:09 +0100

    Update setup.py
    Syntax fail

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def setup_package():
         install_requires=[
             "numpy>1.7",
             "scipy",
-            "matplotlib>1.3”,
+            "matplotlib>1.3",
         ],
           
         extras_require = {
