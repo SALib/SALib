Fixed an error that had to do with the fact that I used is instead of ==. Furthermore I included extra tests for each function and the calculation in the sum_distances function is now vectorized

SALib/sample/morris.py
@@ -33,7 +33,7 @@ def sample(problem, N, num_levels, grid_jump, optimal_trajectories=None, local_o
     
     - Vanilla Morris
     - Optimised trajectories when optimal_trajectories is set (using 
-      Campolongo's enhancements from 2007)
+      Campolongo's enhancements from 2007 and optionally Ruano's enhancement from 2012)
     - Groups with optimised trajectories when optimal_trajectores is set and 
       the problem definition specifies groups
     
@@ -44,6 +44,8 @@ def sample(problem, N, num_levels, grid_jump, optimal_trajectories=None, local_o
     results in an exponentially increasing number of scores that must be
     computed to find the optimal combination of trajectories.  We suggest going
     no higher than 4 from a pool of 100 samples.
+    
+    Update: With local_optimization = True, it is possible to go higher than the previously suggested 4 from 100.
     
     Parameters
     ----------
@@ -57,6 +59,10 @@ def sample(problem, N, num_levels, grid_jump, optimal_trajectories=None, local_o
         The grid jump size
     optimal_trajectories : int
         The number of optimal trajectories to sample (between 2 and N)
+    local_optimization : bool
+        Flag whether to use local optimization according to Ruano et al. (2012) 
+        Speeds up the process tremendously for bigger N and num_levels.
+        Stating this variable to be true causes the function to ignore gurobi.
     """
     if grid_jump >= num_levels:
         raise ValueError("grid_jump must be less than num_levels")
@@ -164,7 +170,7 @@ def compute_optimised_trajectories(problem, input_sample, N, k_choices, local_op
     if np.any((input_sample < 0) | (input_sample > 1)):
         raise ValueError("Input sample must be scaled between 0 and 1")
     
-    if _has_gurobi:
+    if _has_gurobi == True and local_optimization == False:
         maximum_combo = return_max_combo(input_sample,
                                          N,
                                          num_params,
@@ -204,6 +210,8 @@ if __name__ == "__main__":
                         default=2, help='Grid jump size (Morris only)')
     parser.add_argument('-k', '--k-optimal', type=int, required=False,
                         default=None, help='Number of optimal trajectories (Morris only)')
+    parser.add_argument('-o', '--local', type=bool, required=True, 
+                        default=False, help='Use the local optimisation method (Morris with optimization only)')
     args = parser.parse_args()
 
     np.random.seed(args.seed)
@@ -211,7 +219,7 @@ if __name__ == "__main__":
 
     problem = read_param_file(args.paramfile)
     param_values = sample(problem, args.samples, args.levels, \
-                    args.grid_jump, args.k_optimal)
+                    args.grid_jump, args.k_optimal, args.local)
 
     np.savetxt(args.output, param_values, delimiter=args.delimiter,
                fmt='%.' + str(args.precision) + 'e')

SALib/sample/morris_util.py
@@ -13,7 +13,6 @@ from scipy.spatial.distance import cdist
 
 import numpy as np
 import random as rd
-import math
 
 def generate_trajectory(G, num_levels, grid_jump):
     '''
@@ -136,9 +135,11 @@ def compute_distance_matrix(input_sample, N, num_params, groups=None, local_opti
         input_1 = input_sample[index_list[j]]
         for k in range(j + 1, N):
             input_2 = input_sample[index_list[k]]
+            
+            if local_optimization is True:
+                distance_matrix[j, k] = compute_distance(input_1, input_2)
+            
             distance_matrix[k, j] = compute_distance(input_1, input_2)
-            if local_optimization is not False:
-                distance_matrix[j, k] = compute_distance(input_1, input_2) 
     return distance_matrix
 
 
@@ -251,9 +252,6 @@ def compile_output(input_sample, N, num_params, maximum_combo, groups=None):
         output[index_list[counter]] = np.array(input_sample[index_list[x]])
     return output
 
-# method of Ruano et al.
-# defined num_params as number of factors in model
-# defined k_choices as number of desired trajectories
 def sum_distances(indices, distance_matrix):
     '''Calculate combinatorial distance between a select group of trajectories, indicated by indices
     
@@ -266,23 +264,28 @@ def sum_distances(indices, distance_matrix):
     The calculated distances, as they are right now, are only used in a relative way. 
     Purely summing distances would lead to the same result, at a perhaps quicker rate.
     '''  
-    combs = np.array(tuple(combinations(indices, 2)))
-    dist = 0.0
-    
-    for comb in combs:
-        dist += distance_matrix[comb[0],comb[1]]**2
-        
-    return math.sqrt(dist)
+    combs_tup = np.array(tuple(combinations(indices, 2)))
 
+    #Put indices from tuples into two-dimensional array.
+    combs = np.array([[i[0] for i in combs_tup], [i[1] for i in combs_tup]])
 
-def get_max_sum_ind(indices_list, distances):
+    #Calculate distance (vectorized)
+    dist = np.sqrt(np.sum(np.square(distance_matrix[combs[0],combs[1]]), axis=0))
+
+    return dist
+
+def get_max_sum_ind(indices_list, distances, i, m):
     '''Get the indices that belong to the maximum distance in an array of distances
     
     indices_list = list of tuples
     distance = array (M)
+    i = int
+    m = int
     '''
-    if len(indices_list) is not len(distances):
-        raise ValueError("indices and distances are lists of different length")
+    if len(indices_list) != len(distances):
+        raise ValueError("Indices and distances are lists of different length." +
+        "Length indices_list = " + str(len(indices_list)) + " and length distances = " + 
+        str(len(distances)) + ". In loop i = " + str(i) + " and m =  " + str(m))
     
     max_index = tuple(distances.argsort()[-1:][::-1])
     return indices_list[max_index[0]]
@@ -303,7 +306,8 @@ def add_indices(indices, distance_matrix):
 def find_local_maximum(input_sample, N, num_params, k_choices, groups = None):
     '''An alternative by Ruano et al. (2012) for the brute force approach as 
     originally proposed by Campolongo et al. (2007). The method should improve 
-    the speed with which an optimal set of trajectories is found tremendously.
+    the speed with which an optimal set of trajectories is found tremendously 
+    for larger sample sizes.
     '''
     
     if groups is not None:
@@ -328,7 +332,7 @@ def find_local_maximum(input_sample, N, num_params, k_choices, groups = None):
             row_nr += 1
         
         #Find the indices belonging to the maximum distance 
-        i_max_ind = get_max_sum_ind(indices_list,row_maxima_i)
+        i_max_ind = get_max_sum_ind(indices_list,row_maxima_i, i, 0)
 
         #########Loop 'm' (called loop 'k' in Ruano)############
         m_max_ind = i_max_ind
@@ -343,22 +347,24 @@ def find_local_maximum(input_sample, N, num_params, k_choices, groups = None):
             for n in range(0,len(m_ind)):
                 m_maxima[n] = sum_distances(m_ind[n], distance_matrix)  
             
-            m_max_ind = get_max_sum_ind(m_ind, m_maxima)
+            m_max_ind = get_max_sum_ind(m_ind, m_maxima, i, m)
             
             m += 1
         
         tot_indices_list.append(m_max_ind)
         tot_max_array[i-1] = sum_distances(m_max_ind, distance_matrix)
     
-    tot_max = get_max_sum_ind(tot_indices_list, tot_max_array)
+    tot_max = get_max_sum_ind(tot_indices_list, tot_max_array, "tot", "tot")
     return sorted(list(tot_max))
 
-#end method
+
 
 def find_optimum_combination(input_sample, N, num_params, k_choices, groups=None, local_optimization=False):
 
-    if local_optimization is False:
+    if local_optimization is True:
+        maximum_combo = find_local_maximum(input_sample, N, num_params, k_choices, groups)
         
+    else:
         scores = find_most_distant(input_sample,
                                    N,
                                    num_params,
@@ -370,8 +376,5 @@ def find_optimum_combination(input_sample, N, num_params, k_choices, groups=None
         maximum_combo = find_maximum(scores, N, k_choices)
     
         #print(maximum_combo)
-
-    else:
-        maximum_combo = find_local_maximum(input_sample, N, num_params, k_choices, groups)
 
     return maximum_combo

SALib/tests/test_morris_util.py
@@ -16,7 +16,8 @@ from SALib.sample.morris_util import generate_P_star, \
                                 find_most_distant, find_maximum, \
                                 make_index_list, \
                                 check_input_sample, \
-                                find_local_maximum
+                                find_local_maximum, \
+                                sum_distances, get_max_sum_ind, add_indices
 
 
 def setup():
@@ -135,6 +136,60 @@ def test_compute_distance_matrix():
     expected[5, :] = [7.52, 5.99, 5.52, 7.31, 5.77, 0]
     assert_allclose(output, expected, rtol=1e-2)
 
+def test_compute_distance_matrix_local():
+    '''
+    Tests that a distance matrix is computed correctly for the local distance optimization.
+    The only change is that the local method needs the upper triangle of 
+    the distance matrix instead of the lower one.
+    
+    This is for an input of six trajectories and two parameters
+    '''
+    sample_inputs = setup()
+    output = compute_distance_matrix(sample_inputs, 6, 2, local_optimization=True)
+    expected = np.zeros((6, 6), dtype=np.float32)
+    expected[0, :] = [0,    5.50, 6.18, 6.89, 6.18, 7.52]
+    expected[1, :] = [5.50, 0,    5.31, 6.18, 5.31, 5.99]
+    expected[2, :] = [6.18, 5.31, 0,    6.57, 5.41, 5.52]
+    expected[3, :] = [6.89, 6.18, 6.57, 0,    5.50, 7.31]
+    expected[4, :] = [6.18, 5.31, 5.41, 5.5,  0,    5.77]
+    expected[5, :] = [7.52, 5.99, 5.52, 7.31, 5.77, 0   ]
+    assert_allclose(output, expected, rtol=1e-2)
+
+def test_sum_distances():
+    '''
+    Tests whether the combinations are summed correctly. 
+    '''
+    sample_inputs = setup()
+    dist_matr = compute_distance_matrix(sample_inputs, 6, 2, local_optimization=True)
+    indices = (1,3,2)
+    distance = sum_distances(indices, dist_matr)
+
+    expected = 10.47
+    assert_allclose(distance, expected, rtol=1e-2)
+    
+def test_get_max_sum_ind():
+    '''
+    Tests whether the right maximum indices are returned.
+    '''
+    indices = np.array([(1,2,4),(3,2,1),(4,2,1)])
+    distances = np.array([20, 40, 50])
+    
+    output = get_max_sum_ind(indices, distances, 0, 0)
+    expected = (4,2,1)    
+    
+    assert_equal(output, expected)
+
+def test_add_indices():
+    '''
+    Tests whether the right indices are added.
+    '''
+    indices = (1,3,4)
+    matr = np.zeros((6,6), dtype = np.int16)
+    ind_extra = add_indices(indices, matr)
+
+    expected = [(1,3,4,0),(1,3,4,2),(1,3,4,5)]
+    
+    assert_equal(ind_extra, expected)
 
 def test_combo_from_find_most_distant():
     '''
@@ -229,6 +284,12 @@ def test_make_index_list_with_groups():
     desired = [np.array([0, 1, 2]), np.array([3, 4, 5]), np.array([6, 7, 8]), np.array([9, 10, 11])]
     assert_equal(desired, actual)
 
+@raises(ValueError)
+def test_get_max_sum_ind_Error():
+    indices = [(1,2,4),(3,2,1),(4,2,1)]
+    distances_wrong = [20,40]
+    
+    get_max_sum_ind(indices, distances_wrong, 0, 0)
 
 @raises(AssertionError)
 def test_check_input_sample_N():