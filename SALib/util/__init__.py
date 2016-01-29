2bf6e3db3db9d3a36bda1ddeb6f2bbbb681d9509
Calvin Whealton <caw324@cornell.edu>
2016-01-29 12:25:59 -0500

    Changed function for non-uniform scaling and reading-in problem file
    Revised code to make conversion to non-uniform distributions a separate
    function. This restores the initial ‘scale_samples’ function and adds a
    new function, ‘nonuniform_scale_samples’. The change was made so that
    functions that called ‘scale_samples’ would not need to account for any
    changes to that function.
    
    The function ‘read_param_file’ was modified so that it will accept a
    fifth column for the distribution name. The new input files must have
    ‘NA’ listed for groups when no grouping is required but there are
    distributions specified. The function adds a ‘dists’ variable to the
    problem dictionary, and when no distributions are specified the value
    is None.

--- SALib/util/__init__.py
+++ SALib/util/__init__.py
@@ -6,7 +6,7 @@ from warnings import warn
 import numpy as np
 import scipy as sp
 
-def scale_samples(params, bounds, dists=None):
+def scale_samples(params, bounds, dists):
     '''
     Rescales samples in 0-to-1 range to arbitrary bounds.
 
@@ -14,108 +14,24 @@ def scale_samples(params, bounds, dists=None):
         bounds - list of lists of dimensions num_params-by-2
         params - numpy array of dimensions num_params-by-N,
         where N is the number of samples
-        dists - vector of distribution names, None implies uniform
     '''
-    if dists == None:
-        # computations when only uniform distributoins are used
-		
-        # Check bounds are legal (upper bound is greater than lower bound)
-        b = np.array(bounds)
-        lower_bounds = b[:, 0]
-        upper_bounds = b[:, 1]
-		
-        if np.any(lower_bounds >= upper_bounds):
-            raise ValueError("Bounds are not legal")
-		
-        # This scales the samples in-place, by using the optional output
-        # argument for the numpy ufunctions
-        # The calculation is equivalent to:
-        #   sample * (upper_bound - lower_bound) + lower_bound
-        np.add(np.multiply(params,
-                           (upper_bounds - lower_bounds),
-                           out=params),
-               lower_bounds,
-               out=params)
-	
-    else:
-    	# computations for non-uniform distributions
-    	
-        # initializing matrix to hold converted values
-        conv_param =  np.empty([len(params),len(params[0])])
-		
-        if len(dists) != len(params[0]):
-            print('Invalid number of distributions')
-			
-        for i in range(len(dists)):
-            # loop over variables and converting them one by one into specified distributions
-            
-            # uniform distribution
-            if dists[i] == 'unif':
-                arg1 = bounds[i][0] # lower bound
-                arg2 = bounds[i][1] # upper bound
-                
-                # printing error when upper bound is less than lower bound
-                if arg1 > arg2:
-                    print('Upper bound must be less than lower bound')
-                else:
-                    conv_param[:,i] = params[:,i]*(arg2-arg1) + arg1
-					
-			# normal distribution
-            elif dists[i] == 'norm':
-                arg1 = bounds[i][0] # mean
-                arg2 = bounds[i][1] # standard deviation
-                
-                # printing error when standard deviation is not positive
-                if arg2 <= 0:
-                    print('Standard deviation must be positive')
-                else:
-                    conv_param[:,i] = sp.stats.norm.ppf(params[:,i],loc=arg1,scale=arg2)
-					
-			# log-normal distribution (base-e not base-10)
-            elif dists[i] == 'lognorm':
-                arg1 = bounds[i][0] # ln-space mean
-                arg2 = bounds[i][1] # ln-space standard deviation
-                
-                # checking if real-space lower bound is specified
-                if(len(bound[i]) == 3):
-                    arg3 = bounds[i][2]
-                else:
-                    arg3 = 0. # lower bound
-                
-                # printing error if log-space standard deviation is not positive
-                if arg2[i] <= 0:
-                    print('Standard deviation must be positive')
-                else:
-                    conv_param[:,i] = exp(sp.stats.norm.ppf(params[:,i],loc=arg1,scale=arg2))+arg3
-			
-			# triangular distribution
-            elif dists[i] == 'triang':
-                arg1 = bounds[i][0] # lower bound
-                arg2 = bounds[i][1] # upper bound
-                
-                # condition is most-likely value specified
-                # if not specified, use average of upper and lower bounds
-                if(len(bound[i]) == 3):
-                    arg3 = bounds[i][2] # most likely
-                else:
-                    arg3 = np.mean([arg1,arg2]) # lower bound
-                
-                # checking upper bound less than lower bound
-                # and that most likely value within the bounds
-                if arg1 > arg2:
-                    print('Upper bound must be greater than lower bound')
-                elif (arg3 < arg1) or (arg3 > arg2):
-                    print('Most likely value must be within upper and lower bounds')
-                else:
-                    a = arg1 # location (lower bound)
-                    b = arg2 - arg1 # scale (width of distribution)
-                    c = (arg3-a)/b # most likely as percentage of scale
-                    conv_param[:,i] = sp.stats.triang.ppf(params[:,i],c=c,loc=a,scale=b)
-            
-            # error message for a distribution type that is not valid
-            else:
-                print('Not a valid distribution type')
-    	return(conv_param)
+    # Check bounds are legal (upper bound is greater than lower bound)
+    b = np.array(bounds)
+    lower_bounds = b[:, 0]
+    upper_bounds = b[:, 1]
+
+    if np.any(lower_bounds >= upper_bounds):
+        raise ValueError("Bounds are not legal")
+
+    # This scales the samples in-place, by using the optional output
+    # argument for the numpy ufunctions
+    # The calculation is equivalent to:
+    #   sample * (upper_bound - lower_bound) + lower_bound
+    np.add(np.multiply(params,
+                       (upper_bounds - lower_bounds),
+                       out=params),
+           lower_bounds,
+           out=params)
 
 def unscale_samples(params, bounds):
     '''
@@ -142,12 +58,98 @@ def unscale_samples(params, bounds):
               np.subtract(upper_bounds, lower_bounds),
               out=params)
 
+def nonuniform_scale_samples(params, bounds, dists):
+    '''
+    Rescales samples in 0-to-1 range to other distributions.
+
+    Arguments:
+        problem - problem definition including bounds
+        params - numpy array of dimensions num_params-by-N,
+            where N is the number of samples
+        dists-list of distributions, one for each parameter
+                unif: uniform with lower and upper bounds
+                triang: triangular with width (scale) and location of peak
+                        location of peak is in percentage of width
+                        lower bound assumed to be zero
+                norm: normal distribution with mean and standard deviation
+                lognorm: lognormal with ln-space mean and standard deviation
+    '''
+    b = np.array(bounds)
+    
+    if len(params[0]) != len(dists):
+        print('Incorrect number of distributions specified')
+        print('Original parameters returned')
+        return params
+    else:
+        # initializing matrix for converted values
+        conv_params = np.empty([len(params),len(params[0])])
+
+        # loop over the parameters
+        for i in range(len(conv_params[0])):
+            # setting first and second arguments for distributions
+            arg1 = b[i][0]
+            arg2 = b[i][1]
+
+            # triangular distribution
+            # paramters are width (scale) and location of peak
+            # location of peak is relative to scale
+            # e.g., 0.25 means peak is 25% of the width distance from zero
+            if dists[i] == 'triang':
+                # checking for correct parameters
+                if arg1 <= 0:
+                    print('Scale must be greater than zero')
+                    print('Parameter not converted')
+                    conv_params[:,i] = params[:,i]
+                elif (arg2 <= 0) or (arg2 >= 1):
+                    print('Peak must be on interval [0,1]')
+                    print('Parameter not converted')
+                    conv_params[:,i] = params[:,i]
+                else:
+                    conv_params[:,i] = sp.stats.triang.ppf(params[:,i],c=arg2,scale=arg1,loc=0)
+
+            # uniform distribution
+            # parameters are lower and upper bounds
+            elif dists[i] == 'unif':
+                # checking that upper bound is greater than lower bound
+                if arg1 >= arg2:
+                    print('Lower bound greater than upper bound')
+                    print('Parameter not converted')
+                    conv_params[:,i] = params[:,i]
+                else:
+                    conv_params[:,i] = params[:,i]*(arg2-arg1) + arg1
+
+            # normal distribution
+            # paramters are mean and standard deviation
+            elif dists[i] == 'norm':
+                # checking for valid parameters
+                if arg2 <= 0:
+                    print('Scale must be greater than zero')
+                    print('Parameter not converted')
+                    conv_params[:,i] = params[:,i]
+                else:
+                    conv_params[:,i] = sp.stats.norm.ppf(params[:,i],loc=arg1,scale=arg2)
+
+            # lognormal distribution (ln-space, not base-10)
+            # paramters are ln-space mean and standard deviation
+            elif dists[i] == 'lognorm':
+                # checking for valid parameters
+                if arg2 <= 0:
+                    print('Scale must be greater than zero')
+                    print('Parameter not converted')
+                    conv_params[:,i] = params[:,i]
+                else:
+                    conv_params[:,i] = np.exp(sp.stats.norm.ppf(params[:,i],loc=arg1,scale=arg2))
+
+            else:
+                print('No valid distribution selected')
+    return(conv_params)
+
 def read_param_file(filename, delimiter=None):
     '''
     Reads a parameter file of format:
-        Param1,0,1,Group1
-        Param2,0,1,Group2
-        Param3,0,1,Group3
+        Param1,0,1,Group1,dist1
+        Param2,0,1,Group2,dist2
+        Param3,0,1,Group3,dist3
     And returns a dictionary containing:
         - names - the names of the parameters
         - bounds - a list of lists of lower and upper bounds
@@ -156,12 +158,16 @@ def read_param_file(filename, delimiter=None):
         - groups - a tuple containing i) a group matrix assigning parameters to
                    groups
                                       ii) a list of unique group names
+        - dists - a list of distributions for the problem,
+                    None if not specified or all uniform
     '''
     names = []
     bounds = []
     group_list = []
+    dist_list = []
     num_vars = 0
-    fieldnames = ['name', 'lower_bound', 'upper_bound', 'group']
+    fieldnames = ['name', 'lower_bound', 'upper_bound', 'group', 'dist']
+    dist_none_count = 0 # used when evaluating if non-uniform distributions are specified
 
     with open(filename, 'rU') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(1024), delimiters=delimiter)
@@ -181,19 +187,38 @@ def read_param_file(filename, delimiter=None):
                 # the parameter name
                 if row['group'] is None:
                     group_list.append(row['name'])
+                elif row['group'] is 'NA':
+                    group_list.append(row['name'])
                 else:
                     group_list.append(row['group'])
 
-        group_matrix, group_names = compute_groups_from_parameter_file(
-            group_list, num_vars)
+                # If the fifth column does not contain a distribution
+                # use uniform
+                if row['dist'] is None:
+                    dist_list.append('unif')
+                    dist_none_count += 1
+                else:
+                    dist_list.append(row['dist'])
 
-        if np.all(group_matrix == np.eye(num_vars)):
-            group_tuple = None
-        else:
-            group_tuple = (group_matrix, group_names)
+    group_matrix, group_names = compute_groups_from_parameter_file(
+        group_list, num_vars)
+
+    # setting group_tuple to zero if no groups are defined
+    # or all groups are 'NA'
+    if np.all(group_matrix == np.eye(num_vars)):
+        group_tuple = None
+    elif len(np.unique(group_list)) == 1:
+        group_tuple = None
+    else:
+        group_tuple = (group_matrix, group_names)
+
+    # setting dist list to none if all are uniform
+    # because non-uniform scaling is not needed
+    if dist_none_count == num_vars:
+        dist_list = None
 
     return {'names': names, 'bounds': bounds, 'num_vars': num_vars,
-            'groups': group_tuple}
+            'groups': group_tuple, 'dists': dist_list}
 
 
 def compute_groups_from_parameter_file(group_list, num_vars):
