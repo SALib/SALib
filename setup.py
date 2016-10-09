6f1e003fe2db1a33b1e626e07b6e4e3ca8ae6e2f
Will Usher <william.usher@ouce.ox.ac.uk>
2016-10-09 11:47:23 +0100

    Added pyscaffold to replace versioneer

--- setup.py
+++ setup.py
@@ -1,67 +1,22 @@
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for salib.
 
-import versioneer
-setup(version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass())
+    This file was generated with PyScaffold 2.5.5.post0.dev2+gf5d2c64, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
+"""
 
-
-class NoseTestCommand(TestCommand):
-
-    def run_tests(self):
-        # Run nose ensuring that argv simulates running nosetests directly
-        import nose
-        nose.run_exit(argv=['nosetests'])
+import sys
+from setuptools import setup
 
 
 def setup_package():
-    # Assemble additional setup commands
-    cmdclass = versioneer.get_cmdclass()
-    cmdclass['test'] = NoseTestCommand
-
-    setup(
-        name='SALib',
-        packages=find_packages(exclude=["*tests*"]),
-        author="Jon Herman and Will Usher",
-        author_email="jdherman8@gmail.com",
-        license=open('LICENSE.md').read(),
-        tests_require=['nose'],
-        install_requires=[
-            "numpy>=1.9.0",
-            "scipy",
-            "matplotlib>=1.4.3",
-        ],
-
-        extras_require = {
-                          "gurobipy": ["gurobipy",]
-                          },
-
-        # Two arguments required by Versioneer
-        version = versioneer.get_version(),
-        cmdclass=cmdclass,
-        url="https://github.com/SALib/SALib",
-        long_description=open('README.md').read(),
-        description=(
-            'Tools for sensitivity analysis. Contains Sobol, Morris, and FAST methods.'),
-        # entry_points = {
-        #     'console_scripts': [
-        #         'salib = SALib.bin.salib:main',
-        #     ]
-        # },
-        classifiers=[
-            # 'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: End Users/Desktop',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            # 'License :: OSI Approved :: BSD License',
-            'Operating System :: MacOS :: MacOS X',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: POSIX',
-            'Programming Language :: Python',
-            # 'Programming Language :: Python :: 2.7',
-            # 'Programming Language :: Python :: 2 :: Only',
-            'Topic :: Scientific/Engineering :: Mathematics',
-        ],)
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+          use_pyscaffold=True)
 
 
 if __name__ == "__main__":
