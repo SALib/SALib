00868a5dc66b2bec24ebbcb613fa12a58ca55e6f
Will Usher <william.usher@ouce.ox.ac.uk>
2017-06-20 11:13:43 +0100

    Changed default random seed on command-line to 'None', which uses random data

--- SALib/sample/common_args.py
+++ SALib/sample/common_args.py
@@ -6,13 +6,16 @@ def create():
         description='Create parameter samples for sensitivity analysis')
 
     parser.add_argument(
-        '-p', '--paramfile', type=str, required=True, help='Parameter Range File')
+        '-p', '--paramfile', type=str, required=True,
+        help='Parameter Range File')
     parser.add_argument(
         '-o', '--output', type=str, required=True, help='Output File')
     parser.add_argument(
-        '-s', '--seed', type=int, required=False, default=1, help='Random Seed')
+        '-s', '--seed', type=int, required=False, default=None,
+        help='Random Seed')
     parser.add_argument(
-        '--delimiter', type=str, required=False, default=' ', help='Column delimiter')
+        '--delimiter', type=str, required=False, default=' ',
+        help='Column delimiter')
     parser.add_argument('--precision', type=int, required=False,
                         default=8, help='Output floating-point precision')
     return parser
