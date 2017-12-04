eca413844bea9055620963a16a2a6a495a4221a1
Filippo Savi <ssxfs1@nottingham.edu.cn>
2017-12-04 13:57:32 +0100

    removed dead code and fixed docstrings

--- SALib/util/__init__.py
+++ SALib/util/__init__.py
@@ -16,19 +16,11 @@ def scale_samples(params,problem):
 
     Arguments
     ---------
-    bounds : list
-        list of lists of dimensions `num_params`-by-2
     params : numpy.ndarray
         numpy array of dimensions `num_params`-by-:math:`N`,
         where :math:`N` is the number of samples
-    dists : list
-        list of distributions, one for each parameter
-            unif: uniform with lower and upper bounds
-            triang: triangular with width (scale) and location of peak
-                    location of peak is in percentage of width
-                    lower bound assumed to be zero
-            norm: normal distribution with mean and standard deviation
-            lognorm: lognormal with ln-space mean and standard deviation
+    problem : dict
+        The problem definition
     '''
     bounds = problem['bounds']
     try:
@@ -123,80 +115,6 @@ def unscale_samples(params, bounds):
     np.divide(np.subtract(params, lower_bounds, out=params),
               np.subtract(upper_bounds, lower_bounds),
               out=params)
-
-
-def nonuniform_scale_samples(params, bounds, dists):
-    """Rescale samples in 0-to-1 range to other distributions
-
-    Arguments
-    ---------
-    problem : dict
-        problem definition including bounds
-    params : numpy.ndarray
-        numpy array of dimensions num_params-by-N,
-        where N is the number of samples
-    dists : list
-        list of distributions, one for each parameter
-            unif: uniform with lower and upper bounds
-            triang: triangular with width (scale) and location of peak
-                    location of peak is in percentage of width
-                    lower bound assumed to be zero
-            norm: normal distribution with mean and standard deviation
-            lognorm: lognormal with ln-space mean and standard deviation
-    """
-
-    b = np.array(bounds)
-
-    # initializing matrix for converted values
-    conv_params = np.zeros_like(params)
-
-    # loop over the parameters
-    for i in range(conv_params.shape[1]):
-        # setting first and second arguments for distributions
-        b1 = b[i][0]
-        b2 = b[i][1]
-
-        if dists[i] == 'triang':
-            # checking for correct parameters
-            if b1 <= 0 or b2 <= 0 or b2 >= 1:
-                raise ValueError('''Triangular distribution: Scale must be
-                    greater than zero; peak on interval [0,1]''')
-            else:
-                conv_params[:, i] = stats.triang.ppf(
-                    params[:, i], c=b2, scale=b1, loc=0)
-
-        elif dists[i] == 'unif':
-            if b1 >= b2:
-                raise ValueError('''Uniform distribution: lower bound
-                    must be less than upper bound''')
-            else:
-                conv_params[:, i] = params[:, i] * (b2 - b1) + b1
-
-        elif dists[i] == 'norm':
-            if b2 <= 0:
-                raise ValueError('''Normal distribution: stdev must be > 0''')
-            else:
-                conv_params[:, i] = stats.norm.ppf(
-                    params[:, i], loc=b1, scale=b2)
-
-        # lognormal distribution (ln-space, not base-10)
-        # paramters are ln-space mean and standard deviation
-        elif dists[i] == 'lognorm':
-            # checking for valid parameters
-            if b2 <= 0:
-                raise ValueError(
-                    '''Lognormal distribution: stdev must be > 0''')
-            else:
-                conv_params[:, i] = np.exp(
-                    stats.norm.ppf(params[:, i], loc=b1, scale=b2))
-
-        else:
-            valid_dists = ['unif', 'triang', 'norm', 'lognorm']
-            raise ValueError('Distributions: choose one of %s' %
-                             ", ".join(valid_dists))
-
-    return conv_params
-
 
 def read_param_file(filename, delimiter=None):
     """Unpacks a parameter file into a dictionary
