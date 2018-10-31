32a4645577e92775f4aa118334fd1bd6e5a48172
Takuya Iwanaga <takuyai@gmail.com>
2018-10-31 23:28:16 +1100

    display help when no args used for CLI

--- SALib/scripts/salib.py
+++ SALib/scripts/salib.py
@@ -44,36 +44,40 @@ def parse_subargs(module, parser, method, opts):
     subargs : argparser namespace object
     '''
     module.cli_args(parser)
-    subargs = parser.parse_args([method]+opts)
+    subargs = parser.parse_args([method] + opts)
     return subargs
 
 
-parser = argparse.ArgumentParser(
-    description='SALib - Sensitivity Analysis Library')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='SALib - Sensitivity Analysis Library')
+    subparsers = parser.add_subparsers(help='Sample or Analysis method',
+                                       dest='action')
+    sample_parser = subparsers.add_parser('sample')
+    analyze_parser = subparsers.add_parser('analyze')
 
-subparsers = parser.add_subparsers(help='Sample or Analysis method',
-                                   dest='action')
-sample_parser = subparsers.add_parser('sample')
-analyze_parser = subparsers.add_parser('analyze')
+    # Get list of available samplers and analyzers
+    samplers = avail_approaches(sample)
+    analyzers = avail_approaches(analyze)
+    sample_parser.add_argument('method', choices=samplers)
+    analyze_parser.add_argument('method', choices=analyzers)
 
-# Get list of available samplers and analyzers
-samplers = avail_approaches(sample)
-analyzers = avail_approaches(analyze)
-sample_parser.add_argument('method', choices=samplers)
-analyze_parser.add_argument('method', choices=analyzers)
+    known_args, opts = parser.parse_known_args()
 
-known_args, opts = parser.parse_known_args()
+    action = known_args.action
+    if not action:
+        parser.print_help()
+        import sys
+        sys.exit()
 
-action = known_args.action
-method_name = known_args.method
+    method_name = known_args.method
+    module = importlib.import_module('.'.join(['SALib', action, method_name]))
+    if action == 'sample':
+        parse_obj = sample_parser
+        run_func = module.run_sample
+    elif action == 'analyze':
+        parse_obj = analyze_parser
+        run_func = module.run_analysis
 
-module = importlib.import_module('.'.join(['SALib', action, method_name]))
-if action == 'sample':
-    parse_obj = sample_parser
-    run_func = module.run_sample
-elif action == 'analyze':
-    parse_obj = analyze_parser
-    run_func = module.run_analysis
-
-subargs = parse_subargs(module, parse_obj, method_name, opts)
-run_func(subargs)
+    subargs = parse_subargs(module, parse_obj, method_name, opts)
+    run_func(subargs)
