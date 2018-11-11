f55de960dd7d9bd6be7511576c5df32cc21da21b
Takuya Iwanaga <takuyai@gmail.com>
2018-11-11 19:36:05 +1100

    Corrected test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ import subprocess
 
 def test_cli_usage():
     cmd = ["salib"]
-    out = subprocess.check_output(cmd, shell=True)
+    out = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     out = out.decode()
     assert len(out) > 0 and "usage" in out.lower(), \
         "Incorrect message returned from utility"
@@ -11,15 +11,15 @@ def test_cli_usage():
 
 def test_cli_setup():
     cmd = ["salib", "sample", "morris"]
-    out = subprocess.check_output(cmd, shell=True)
+    out = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     out = out.decode()
     assert len(out) > 0 and "error" not in out.lower(), \
         "Could not use salib as command line utility!"
 
     cmd = ["salib", "sample", "unknown_method"]
-    out = subprocess.check_output(cmd, shell=True)
+    out = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     out = out.decode()
-    assert len(out) > 0 and "error" not in out.lower(), \
+    assert len(str(out)) > 0 and "invalid choice" in out.lower(), \
         "Unimplemented method selected but no error outputted!"
 
 
