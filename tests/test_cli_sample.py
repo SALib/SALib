eeac4a5a42a74eb2c1f71633d9971188e8b95f1e
Takuya Iwanaga <takuyai@gmail.com>
2018-11-17 00:30:27 +1100

    test dataset and updated path

--- tests/test_cli_sample.py
+++ tests/test_cli_sample.py
@@ -1,7 +1,9 @@
+from os.path import join as pth_join
 import subprocess
 
 salib_cli = "../SAlib/scripts/salib.py"
 ishigami_fp = "../SALib/test_functions/params/Ishigami.txt"
+test_data = pth_join('data', 'test.txt')
 
 
 def test_cli_entry():
@@ -10,41 +12,46 @@ def test_cli_entry():
 
 
 def test_ff():
-    cmd = "python {cli} sample ff -p {fn} -r test.txt -n 100".format(
+    cmd = "python {cli} sample ff -p {fn} -r {test_data} -n 100".format(
         cli=salib_cli,
-        fn=ishigami_fp)
+        fn=ishigami_fp,
+        test_data=test_data)
     result = subprocess.check_output(cmd)
     assert len(result) == 0, "Error occurred!"
 
 
 def test_fast():
-    cmd = "python {cli} sample fast_sampler -p {fn} -r test.txt -n 100".format(
+    cmd = "python {cli} sample fast_sampler -p {fn} -r {test_data} -n 100".format(
         cli=salib_cli,
-        fn=ishigami_fp)
+        fn=ishigami_fp,
+        test_data=test_data)
     result = subprocess.check_output(cmd)
     assert len(result) == 0, "Error occurred!"
 
 
 def test_finite_diff():
-    cmd = "python {cli} sample finite_diff -p {fn} -r test.txt -n 100".format(
+    cmd = "python {cli} sample finite_diff -p {fn} -r {test_data} -n 100".format(
         cli=salib_cli,
-        fn=ishigami_fp)
+        fn=ishigami_fp,
+        test_data=test_data)
     result = subprocess.check_output(cmd)
     assert len(result) == 0, "Error occurred!"
 
 
 def test_latin():
-    cmd = "python {cli} sample latin -p {fn} -r test.txt -n 100".format(
+    cmd = "python {cli} sample latin -p {fn} -r {test_data} -n 100".format(
         cli=salib_cli,
-        fn=ishigami_fp)
+        fn=ishigami_fp,
+        test_data=test_data)
     result = subprocess.check_output(cmd)
     assert len(result) == 0, "Error occurred!"
 
 
 def test_saltelli():
-    cmd = "python {cli} sample latin -p {fn} -r test.txt -n 100".format(
+    cmd = "python {cli} sample latin -p {fn} -r {test_data} -n 100".format(
         cli=salib_cli,
-        fn=ishigami_fp)
+        fn=ishigami_fp,
+        test_data=test_data)
     result = subprocess.check_output(cmd)
     assert len(result) == 0, "Error occurred!"
 
