c0b6f81e01ac7ee6eb0e7ef9c2092cb67ac3d48a
Will Usher <william.usher@ouce.ox.ac.uk>
2019-03-14 11:49:44 +0000

    Removed nose dependency from tests, replaced with pytest - fix #212

--- tests/test_sobol.py
+++ tests/test_sobol.py
@@ -1,6 +1,6 @@
 from __future__ import division
 
-from nose.tools import with_setup, eq_, raises
+from pytest import raises
 from numpy.testing import assert_equal, assert_allclose
 import numpy as np
 from scipy.stats import norm
@@ -11,78 +11,91 @@ from SALib.test_functions import Ishigami, Sobol_G
 from SALib.util import read_param_file
 
 
-def setup_samples(N = 500, calc_second_order = True):
+def setup_samples(N=500, calc_second_order=True):
     param_file = 'src/SALib/test_functions/params/Ishigami.txt'
     problem = read_param_file(param_file)
-    param_values = saltelli.sample(problem, N=N, calc_second_order=calc_second_order)
-    return problem,param_values
+    param_values = saltelli.sample(
+        problem, N=N, calc_second_order=calc_second_order)
+    return problem, param_values
 
 
 def test_sobol_sequence():
     # example from Joe & Kuo: http://web.maths.unsw.edu.au/~fkuo/sobol/
-    S = sobol_sequence.sample(10,3)
-    expected = [[0,0,0],[0.5,0.5,0.5],[0.75,0.25,0.25],[0.25,0.75,0.75],
-                [0.375,0.375,0.625],[0.875,0.875,0.125],[0.625,0.125,0.875],
-                [0.125,0.625,0.375],[0.1875,0.3125,0.9375],[0.6875,0.8125,0.4375]]
+    S = sobol_sequence.sample(10, 3)
+    expected = [[0, 0, 0],
+                [0.5, 0.5, 0.5],
+                [0.75, 0.25, 0.25],
+                [0.25, 0.75, 0.75],
+                [0.375, 0.375, 0.625],
+                [0.875, 0.875, 0.125],
+                [0.625, 0.125, 0.875],
+                [0.125, 0.625, 0.375],
+                [0.1875, 0.3125, 0.9375],
+                [0.6875, 0.8125, 0.4375]]
     assert_allclose(S, expected, atol=5e-2, rtol=1e-1)
 
 
 def test_sample_size_second_order():
     N = 500
     D = 3
-    problem,param_values = setup_samples(N=N)
+    problem, param_values = setup_samples(N=N)
     assert_equal(param_values.shape, [N * (2 * D + 2), D])
 
 
 def test_sample_size_first_order():
     N = 500
     D = 3
-    problem,param_values = setup_samples(N=N, calc_second_order=False)
+    problem, param_values = setup_samples(N=N, calc_second_order=False)
     assert_equal(param_values.shape, [N * (D + 2), D])
 
 
-@raises(RuntimeError)
 def test_incorrect_sample_size():
-    problem,param_values = setup_samples()
+    problem, param_values = setup_samples()
     Y = Ishigami.evaluate(param_values)
-    sobol.analyze(problem, Y[:-10], calc_second_order=True)
+    with raises(RuntimeError):
+        sobol.analyze(problem, Y[:-10], calc_second_order=True)
 
 
-@raises(RuntimeError)
 def test_bad_conf_level():
-    problem,param_values = setup_samples()
+    problem, param_values = setup_samples()
     Y = Ishigami.evaluate(param_values)
-    Si = sobol.analyze(problem, Y,
-                       calc_second_order=True, conf_level=1.01, print_to_console=False)
+    with raises(RuntimeError):
+        sobol.analyze(problem, Y,
+                      calc_second_order=True,
+                      conf_level=1.01,
+                      print_to_console=False)
 
 
-@raises(RuntimeError)
 def test_incorrect_second_order_setting():
     # note this will still be a problem if N(2D+2) also divides by (D+2)
-    problem,param_values = setup_samples(N=501, calc_second_order=False)
+    problem, param_values = setup_samples(N=501, calc_second_order=False)
     Y = Ishigami.evaluate(param_values)
-    sobol.analyze(problem, Y, calc_second_order=True)
+    with raises(RuntimeError):
+        sobol.analyze(problem, Y, calc_second_order=True)
 
 
 def test_include_print():
-    problem,param_values = setup_samples()
+    problem, param_values = setup_samples()
     Y = Ishigami.evaluate(param_values)
-    Si = sobol.analyze(problem, Y,
-                       calc_second_order=True, conf_level=0.95, print_to_console=True)
+    sobol.analyze(problem, Y,
+                  calc_second_order=True,
+                  conf_level=0.95,
+                  print_to_console=True)
 
 
 def test_parallel_first_order():
     c2o = False
     N = 10000
-    problem,param_values = setup_samples(N=N, calc_second_order=c2o)
+    problem, param_values = setup_samples(N=N, calc_second_order=c2o)
     Y = Ishigami.evaluate(param_values)
 
-    A,B,AB,BA = sobol.separate_output_values(Y, D=3, N=N,
-                                            calc_second_order=c2o)
+    A, B, AB, BA = sobol.separate_output_values(Y, D=3, N=N,
+                                                calc_second_order=c2o)
     r = np.random.randint(N, size=(N, 100))
     Z = norm.ppf(0.5 + 0.95 / 2)
     tasks, n_processors = sobol.create_task_list(D=3,
-                            calc_second_order=c2o, n_processors=None)
+                                                 calc_second_order=c2o,
+                                                 n_processors=None)
     Si_list = []
     for t in tasks:
         Si_list.append(sobol.sobol_parallel(Z, A, AB, BA, B, r, t))
@@ -95,15 +108,16 @@ def test_parallel_first_order():
 def test_parallel_second_order():
     c2o = True
     N = 10000
-    problem,param_values = setup_samples(N=N, calc_second_order=c2o)
+    problem, param_values = setup_samples(N=N, calc_second_order=c2o)
     Y = Ishigami.evaluate(param_values)
 
-    A,B,AB,BA = sobol.separate_output_values(Y, D=3, N=N,
-                                            calc_second_order=c2o)
+    A, B, AB, BA = sobol.separate_output_values(Y, D=3, N=N,
+                                                calc_second_order=c2o)
     r = np.random.randint(N, size=(N, 100))
     Z = norm.ppf(0.5 + 0.95 / 2)
     tasks, n_processors = sobol.create_task_list(D=3,
-                            calc_second_order=c2o, n_processors=None)
+                                                 calc_second_order=c2o,
+                                                 n_processors=None)
     Si_list = []
     for t in tasks:
         Si_list.append(sobol.sobol_parallel(Z, A, AB, BA, B, r, t))
@@ -111,7 +125,8 @@ def test_parallel_second_order():
 
     assert_allclose(Si['S1'], [0.31, 0.44, 0.00], atol=5e-2, rtol=1e-1)
     assert_allclose(Si['ST'], [0.55, 0.44, 0.24], atol=5e-2, rtol=1e-1)
-    assert_allclose([Si['S2'][0][1], Si['S2'][0][2], Si['S2'][1][2]], [0.00, 0.25, 0.00], atol=5e-2, rtol=1e-1)
+    assert_allclose([Si['S2'][0][1], Si['S2'][0][2], Si['S2'][1][2]], [
+                    0.00, 0.25, 0.00], atol=5e-2, rtol=1e-1)
 
 
 def test_Sobol_G_using_sobol():
@@ -123,7 +138,7 @@ def test_Sobol_G_using_sobol():
     '''
     problem = {'num_vars': 6,
                'names': ['x1', 'x2', 'x3', 'x4', 'x5', 'x6'],
-               'bounds': [[0, 1], [0, 1], [0, 1], [0, 1],[0, 1], [0, 1]]}
+               'bounds': [[0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1]]}
     N = 5000
     a = np.array([78, 12, 0.5, 2, 97, 33])
     param_values = saltelli.sample(problem, N, calc_second_order=False)
