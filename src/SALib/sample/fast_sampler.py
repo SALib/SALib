5fa3371515781ce714c72cb8c1923c5cc2e458a7
ConnectedSystems <takuyai@gmail.com>
2019-09-02 23:12:24 +1000

    Fixed typo
    As reported in Issue #205

--- src/SALib/sample/fast_sampler.py
+++ src/SALib/sample/fast_sampler.py
@@ -12,7 +12,7 @@ def sample(problem, N, M=4, seed=None):
     """Generate model inputs for the Fourier Amplitude Sensitivity Test (FAST).
 
     Returns a NumPy matrix containing the model inputs required by the Fourier
-    Amplitude sensitivity test.  The resulting matrix contains N rows and D
+    Amplitude sensitivity test.  The resulting matrix contains N * D rows and D
     columns, where D is the number of parameters.  The samples generated are
     intended to be used by :func:`SALib.analyze.fast.analyze`.
 
