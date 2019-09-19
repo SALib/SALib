4d1520c019a25807b78080306de30618ee3279d8
ConnectedSystems <takuyai@gmail.com>
2019-09-19 23:28:43 +1000

    Implementation note moved to example section in docstring

--- src/SALib/sample/radial.py
+++ src/SALib/sample/radial.py
@@ -28,6 +28,27 @@ def sample(problem: Dict, N: int,
     seed : int
         Seed value to use for np.random.seed
 
+    Example
+    -------
+    >>> X = sample(problem, N, seed)
+    
+    `X` will now hold:
+    [[x_{1,1}, x_{1,2}, ..., x_{1,p}]
+    [b_{1,1}, x_{1,2}, ..., x_{1,p}]
+    [x_{1,1}, b_{1,2}, ..., x_{1,p}]
+    [x_{1,1}, x_{1,2}, ..., b_{1,p}]
+    ...
+    [x_{N,1}, x_{N,2}, ..., x_{N,p}]
+    [b_{N,1}, x_{N,2}, ..., x_{N,p}]
+    [x_{N,1}, b_{N,2}, ..., x_{N,p}]
+    [x_{N,1}, x_{N,2}, ..., b_{N,p}]]
+
+    where `p` denotes the number of parameters as
+    specified in `problem`
+
+    We can now run the model using the values in `X`. 
+    The total number of model evaluations will be `N(p+1)`.
+
     Returns
     ---------
     numpy.ndarray : An array of samples
@@ -35,25 +56,26 @@ def sample(problem: Dict, N: int,
     if seed:
         np.random.seed(seed)
 
-    p = problem['num_vars']
+    num_vars = problem['num_vars']
     bounds = problem['bounds']
 
     # Generate the "nominal" values and their perturbations.
-    # "We obtain good results by systematically discarding four points (R = r + 4)" (p 5)
-    # in this context, `r := N`
+    # "We obtain good results by systematically discarding four points (R = r + 4)" 
+    # (Campolongo et al. 2011, p 5)
+    # in this context, `N := r`
     discard = 4
     R = N + discard
 
     # Generate the 'nominal' parameter positions
     # Total number of parameter sets = N*(p+1)
-    base_sequence = sobol_sequence.sample(R, p)
+    base_sequence = sobol_sequence.sample(R, num_vars)
     subsetted_base = base_sequence[discard:]
     scale_samples(subsetted_base, bounds)
 
-    group = p+1
+    group = num_vars+1
     sample_set = np.repeat(subsetted_base, repeats=group, axis=0)
 
-    perturbations = sobol_sequence.sample(R+N, p)
+    perturbations = sobol_sequence.sample(R+N, num_vars)
     perturbations = perturbations[R:]
     scale_samples(perturbations, bounds)
 
@@ -61,23 +83,8 @@ def sample(problem: Dict, N: int,
     for i in range(perturbations.shape[0]):
         mod = np.diag(perturbations[i])
 
-        np.copyto(sample_set[grp_start+1:grp_start+p+1], mod, where=mod != 0.0)
-        grp_start += p+1
+        np.copyto(sample_set[grp_start+1:grp_start+num_vars+1], mod, where=mod != 0.0)
+        grp_start += num_vars + 1
     # End for
-
-    # The above will look like:
-    # [x_{1,1}, x_{1,2}, ..., x_{1,p}]
-    # [b_{1,1}, x_{1,2}, ..., x_{1,p}]
-    # [x_{1,1}, b_{1,2}, ..., x_{1,p}]
-    # [x_{1,1}, x_{1,2}, ..., b_{1,p}]
-    # ...
-    # [x_{N,1}, x_{N,2}, ..., x_{N,p}]
-    # [b_{N,1}, x_{N,2}, ..., x_{N,p}]
-    # [x_{N,1}, b_{N,2}, ..., x_{N,p}]
-    # [x_{N,1}, x_{N,2}, ..., b_{N,p}]
-
-    # We can now run the model using the values from the
-    # sample set. The total number of model evaluations
-    # will be `N(p+1)`.
 
     return sample_set
