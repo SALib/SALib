45af95c94ed90ddc35b433c099d344911809c82d
ConnectedSystems <takuyai@gmail.com>
2020-04-19 13:48:53 +1000

    Morris - use dataframe printing

--- src/SALib/analyze/morris.py
+++ src/SALib/analyze/morris.py
@@ -113,29 +113,11 @@ def analyze(problem, X, Y,
         Si['mu_star_conf'][j] = compute_mu_star_confidence(
             ee[j, :], num_trajectories, num_resamples, conf_level)
 
-    if groups is None:
-        if print_to_console:
-            print("{0:<30} {1:>10} {2:>10} {3:>15} {4:>10}".format(
-                "Parameter",
-                "Mu_Star",
-                "Mu",
-                "Mu_Star_Conf",
-                "Sigma")
-            )
-            for j in list(range(num_vars)):
-                print("{0:30} {1:10.3f} {2:10.3f} {3:15.3f} {4:10.3f}".format(
-                    Si['names'][j],
-                    Si['mu_star'][j],
-                    Si['mu'][j],
-                    Si['mu_star_conf'][j],
-                    Si['sigma'][j])
-                )
-        return Si
-    elif groups is not None:
+    if groups is not None:
         # if there are groups, then the elementary effects returned need to be
         # computed over the groups of variables,
         # rather than the individual variables
-        Si_grouped = dict((k, [None] * num_vars)
+        Si_grouped = ResultDict((k, [None] * num_vars)
                           for k in ['mu_star', 'mu_star_conf'])
         Si_grouped['mu_star'] = compute_grouped_metric(Si['mu_star'], groups)
         Si_grouped['mu_star_conf'] = compute_grouped_metric(Si['mu_star_conf'],
@@ -144,27 +126,12 @@ def analyze(problem, X, Y,
         Si_grouped['sigma'] = compute_grouped_sigma(Si['sigma'], groups)
         Si_grouped['mu'] = compute_grouped_sigma(Si['mu'], groups)
 
-        if print_to_console:
-            print("{0:<30} {1:>10} {2:>10} {3:>15} {4:>10}".format(
-                "Parameter",
-                "Mu_Star",
-                "Mu",
-                "Mu_Star_Conf",
-                "Sigma")
-            )
-            for j in list(range(number_of_groups)):
-                print("{0:30} {1:10.3f} {2:10.3f} {3:15.3f} {4:10.3f}".format(
-                    Si_grouped['names'][j],
-                    Si_grouped['mu_star'][j],
-                    Si_grouped['mu'][j],
-                    Si_grouped['mu_star_conf'][j],
-                    Si_grouped['sigma'][j])
-                )
+        Si = Si_grouped
+    
+    if print_to_console:
+        print(Si.to_df().to_string())
 
-        return Si_grouped
-    else:
-        raise RuntimeError(
-            "Could not determine which parameters should be returned")
+    return Si
 
 
 def compute_grouped_sigma(ungrouped_sigma, group_matrix):
