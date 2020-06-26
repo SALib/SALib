a5c8930027687d49a1bbc246c377545f02c31623
ConnectedSystems <takuyai@gmail.com>
2020-06-26 18:40:09 +1000

    expanded module imports

--- src/SALib/sample/morris/__init__.py
+++ src/SALib/sample/morris/__init__.py
@@ -1,1 +1,6 @@
-from .morris import sample
+from .gurobi import GlobalOptimisation
+from .local import LocalOptimisation
+from .brute import BruteForce
+from .strategy import SampleMorris
+
+from .morris import sample, compute_delta, cli_parse, cli_action
