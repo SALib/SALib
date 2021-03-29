36b478d715f0d9c9f536a23316b3d094625d7b79
ConnectedSystems <takuyai@gmail.com>
2021-03-29 11:37:48 +1100

    Remove unnecessary array creation (omega)

--- src/SALib/analyze/fast.py
+++ src/SALib/analyze/fast.py
@@ -146,12 +146,6 @@ def analyze(problem, Y, M=4, print_to_console=False, seed=None):
     # Recreate the vector omega used in the sampling
     omega_0 = math.floor((N - 1) / (2 * M))
 
-    # m = math.floor(omega_0 / (2 * M))
-    # if m >= (D - 1):
-    #     omega[1:] = np.floor(np.linspace(1, m, D - 1))
-    # else:
-    #     omega[1:] = np.arange(D - 1) % m + 1
-
     # Calculate and Output the First and Total Order Values
     if print_to_console:
         print("Parameter First Total")
@@ -159,8 +153,8 @@ def analyze(problem, Y, M=4, print_to_console=False, seed=None):
     Si['names'] = problem['names']
     for i in range(D):
         l = np.arange(i * N, (i + 1) * N)
-        Si['S1'][i] = compute_first_order(Y[l], N, M, omega[0])
-        Si['ST'][i] = compute_total_order(Y[l], N, omega[0])
+        Si['S1'][i] = compute_first_order(Y[l], N, M, omega_0)
+        Si['ST'][i] = compute_total_order(Y[l], N, omega_0)
         if print_to_console:
             print("%s %f %f" %
                   (problem['names'][i], Si['S1'][i], Si['ST'][i]))
