2fd8d7722c3839619d1dd040cbbdeb75515d3674
ConnectedSystems <takuyai@gmail.com>
2021-05-03 23:43:53 +1000

    Use built-ins to address deprecation messages for numpy v1.2

--- src/SALib/sample/ff.py
+++ src/SALib/sample/ff.py
@@ -107,7 +107,7 @@ def sample(problem, seed=None):
     if seed:
         np.random.seed(seed)
     contrast = generate_contrast(problem)
-    sample = np.array((contrast + 1.) / 2, dtype=np.float)
+    sample = np.array((contrast + 1.) / 2, dtype=float)
     problem = extend_bounds(problem)
     scale_samples(sample, problem['bounds'])
     return sample
