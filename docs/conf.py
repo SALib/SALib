9be8654355ec851e0a3e18014ae253b9b5c10af6
Pamphile Roy <roy.pamphile@gmail.com>
2022-08-18 14:52:24 +0200

    Change theme to pydata-sphinx-theme

--- docs/conf.py
+++ docs/conf.py
@@ -152,12 +152,19 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"sidebar_width": "300px", "page_width": "1200px"}
+html_theme_options = {
+    "github_url": "https://github.com/salib/salib",
+    "navbar_end": ["theme-switcher", "version-switcher", "navbar-icon-links"],
+    "switcher": {
+        "json_url": "https://scipy.github.io/devdocs/_static/version_switcher.json",
+        "version_match": version,
+  }
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
