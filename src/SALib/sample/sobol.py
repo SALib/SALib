7a5be3d5364e8fc0c8c7dc37fc8a23bc5a2d2f58
Takuya Iwanaga <takuyai@gmail.com>
2022-12-25 12:40:07 +1100

    Fix alignment errors for Sobol sample

--- src/SALib/sample/sobol.py
+++ src/SALib/sample/sobol.py
@@ -17,8 +17,7 @@ def sample(
     skip_values: int = 0,
     seed: Optional[Union[int, np.random.Generator]] = None,
 ):
-    """
-    Generates model inputs using Saltelli's extension of the Sobol' sequence.
+    """Generates model inputs using Saltelli's extension of the Sobol' sequence.
 
     The Sobol' sequence is a popular quasi-random low-discrepancy sequence used
     to generate uniform samples of parameter space.
@@ -34,9 +33,8 @@ def sample(
 
     These model inputs are intended to be used with
     :func:`SALib.analyze.sobol.analyze`.
-
-
-    Notes
+	
+	Notes
     -----
     The initial points of the Sobol' sequence has some repetition (see Table 2
     in Campolongo [1]__), which can be avoided by scrambling the sequence.
@@ -48,21 +46,20 @@ def sample(
     increasing the number of samples needed to achieve convergence
     (see Owen [2]__).
 
-
-    Parameters
+	Parameters
     ----------
-    problem : dict
-        The problem definition
+    problem : dict, 
+        The problem definition.
     N : int
-        The number of samples to generate. \
+        The number of samples to generate.
         Ideally a power of 2 and <= `skip_values`.
     calc_second_order : bool, optional
         Calculate second-order sensitivities. Default is True.
     scramble : bool, optional
-        If True, use LMS+shift scrambling. Otherwise, no scrambling is done. \
+        If True, use LMS+shift scrambling. Otherwise, no scrambling is done.
         Default is True.
     skip_values : int, optional
-        Number of points in Sobol' sequence to skip, ideally a value of base 2. \
+        Number of points in Sobol' sequence to skip, ideally a value of base 2.
         It's recommended not to change this value and use `scramble` instead.
         `scramble` and `skip_values` can be used together.
         Default is 0.
@@ -73,12 +70,11 @@ def sample(
         If `seed` is already a ``Generator`` instance then that instance is
         used. Default is None.
 
-
     References
     ----------
     1. Campolongo, F., Saltelli, A., Cariboni, J., 2011.
        From screening to quantitative sensitivity analysis.
-           A unified approach.
+       A unified approach.
        Computer Physics Communications 182, 978-988.
        https://doi.org/10.1016/j.cpc.2010.12.039
 
@@ -95,7 +91,7 @@ def sample(
 
     4. Sobol', I.M., 2001.
        Global sensitivity indices for nonlinear mathematical models and
-           their Monte Carlo estimates.
+       their Monte Carlo estimates.
        Mathematics and Computers in Simulation,
        The Second IMACS Seminar on Monte Carlo Methods 55, 271-280.
        https://doi.org/10.1016/S0378-4754(00)00270-6
@@ -197,7 +193,7 @@ def cli_parse(parser):
     parser : argparse object
 
     Returns
-    ----------
+    -------
     Updated argparse object
     """
     parser.add_argument(
