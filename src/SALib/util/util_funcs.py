ed9c5ad20c3c802139e18d29b91a9b813374d4a9
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2025-09-14 09:48:40 +0200

    code simplification of handle_seed
    in line with numpy docs, Generator.spawn is an equivalent short hand for the complicated code I drew on from scipy

--- src/SALib/util/util_funcs.py
+++ src/SALib/util/util_funcs.py
@@ -188,9 +188,7 @@ def handle_seed(
 
     if isinstance(seed, np.random.Generator):
         # Spawn a Generator that we can own and reset.
-        bg = seed._bit_generator
-        ss = bg._seed_seq
-        rng = [np.random.Generator(type(bg)(child_ss)) for child_ss in ss.spawn(1)][0]
+        rng = seed.spawn(1)
     elif isinstance(
         seed, (np.random.BitGenerator, np.random.SeedSequence, int, type(None))
     ):
